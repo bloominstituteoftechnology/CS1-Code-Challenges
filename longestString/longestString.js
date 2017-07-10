b4722c143a109e7e9aa5396ede7ffdc71bfc61dd
Kayce Hubbard <kayce.hubbard@gmail.com>
2017-07-10 11:58:27 -0500

    First attempt at longestString function.

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,16 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+const longestString = (stringArray) => {
+	let currentLongest = stringArray[0];
+		for(strings of stringArray) {
+			if(strings.length > currentLongest.length) {
+				currentLongest = strings;
+			}
+		}
+	return currentLongest
+}
+
+const array = ['gorm', 'grim', 'grum', 'gram'];
+console.log(longestString(array));
