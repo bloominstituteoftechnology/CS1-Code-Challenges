a11633df61bb6d527a6a59a30ae5aee45c368197
mixelpixel <mixelpix@mac.com>
2017-07-10 12:41:03 -0400

    basic compare for longest string, no edge or corner cases

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,23 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+/* Take the first array item and put it in a "holding" array.
+ * Compare it to the next anArray, put the bigger of the two in the holding array.
+ */
+
+testanArray = ['fred', 'ted', 'bob', 'alice', 'joe', 'arnold', 'mary', 'jebedaiah', 'ed', 'zachahurres', 'mae', 'antidisestablishmentarianism']
+
+const bigString = function (anArr) {
+  const holdBig = [anArr[0]];
+  for (let i = 0; i < anArr.length; i++) {
+    if (holdBig[0].length < anArr[i].length) {
+      holdBig.pop();
+      // console.log(anArr[i]);
+      holdBig.push(anArr[i]);
+    };
+  };
+  return holdBig[0];
+};
+
+console.log(bigString(testanArray));
