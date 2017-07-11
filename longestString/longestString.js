3cd91d189f484ee79d2e1f32137f0f545c9f2f37
Derrick <clickspiker23@gmail.com>
2017-07-11 10:46:23 -0500

    Code challenge for longest string

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,18 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+ const longestString = (arrayToSearch) => {
+   let longest = 0;
+
+   for (let i = 0; i < arrayToSearch.length; i++) {
+     if (arrayToSearch[i]).length > longest) {
+       longest = arrayToSearch[i].length;
+     }
+     return longest;
+   }
+ }
+
+arrayStrings = ["hello", "hola", "Javascript"];
+
+console.log(longestString(arrayStrings));
