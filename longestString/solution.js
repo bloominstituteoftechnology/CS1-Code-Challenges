37a5af6b0f90b45578c06493f0722e408ecfb07e
Ryan Hamblin <ryanhamblin@MacBook-Pro.domain>
2017-07-11 11:40:13 -0600

    adding git changes to the readme and a solution

--- longestString/solution.js
+++ longestString/solution.js
@@ -1,8 +1,9 @@
 const longestString = (strArr) => {
-  // tempStr variable to hold the string
-  // loop over strArr
-  // if current string in arry is bigger than tempStr then update the variable tempStr
-  // return tempStr;
+  let tempStr = '';
+  strArr.forEach(str => {
+    if(str.length > tempStr.length) tempStr = str;
+  });
+  return tempStr;
 };
 
-console.log(longestString(['abc', 'def', 'gasdfasf', 'asdf', 'e', 'agwoaiengpoing']));
+longestString(['abc', 'def', 'gasdfasf', 'asdf', 'e', 'agwoaiengpoing']);
