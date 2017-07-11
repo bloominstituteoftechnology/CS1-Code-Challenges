95800003d2967fea0c440d9d67fc7799f77e8bc7
bdurb <brian@rpmgreenville.com>
2017-07-11 15:47:50 -0400

    test cases pass

---
+++ isUnique/isUnique.js
@@ -0,0 +1,19 @@
+/* 
+* Implement an algorithm to determine if a string has all unique characters.
+* Extra Credit - Answer this question - What if you cannot use additional data structures?
+*/
+
+const isUnique = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i+1; j < str.length; j++) {
+        if (str[i] === str[j]) {
+        return false;
+        }
+      }
+    }
+  return true;
+};
+
+
+console.log(isUnique('abcdhijklmnopqrstuv')); // true
+console.log(isUnique('abcdefga')); // false
