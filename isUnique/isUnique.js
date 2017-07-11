a458dbfad258473b407e05423cd133a1231642e9
wlharvey4 <wlharvey4@mac.com>
2017-07-11 14:59:59 -0700

    isUnique.js initial commit

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -4,8 +4,20 @@
 */
 
 const isUnique = (str) => {
-
+    const set = new Set();
+    const checkSet = (i = 0) => {
+	if (i === str.length) return true;
+	if (set.has(str[i])) return false;
+	set.add(str[i]);
+	return checkSet(++i);
+    }
+    return checkSet();
 };
 
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
 console.log(isUnique('abcdefga')); // false
+
+/*
+If you cannot use additional datastructures, then sort the string and search for
+duplicate letters next to each other.
+*/
