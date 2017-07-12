e0a24ce91c13e836a3d4b1ebe5517ef1a0100b49
Manisha Lal <manishalal972@Manishas-MacBook-Air.local>
2017-07-12 13:25:01 -0500

    Solved

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,17 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+const arr = ['a'];
+arr.push('good');
+arr.push('morning');
+arr.push('everyone');
+arr.push('how');
+arr.push('is');
+arr.push('everyone');
+arr.push('doing');
+
+console.log(arr);
+const longest = arr.reduce((a, b) => a.length >= b.length ? a : b
+);
+
+console.log(longest);
