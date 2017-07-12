f8ae8c8a5604cbcc2760bd487fdd76fd9575311e
mixelpixel <mixelpix@mac.com>
2017-07-12 12:26:15 -0400

    done √

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -5,3 +5,32 @@
  */
 
 
+const reverseCase = function (str) {
+  // declare an array to house individual string letters or spaces in word/sentence order
+  const letters = [];
+  // declare a variable which will be used to reconstructed string
+  let caseConvertedString;
+
+  // examine each letter in the string
+  for (let i = 0; i < str.length; i++) {
+    // uppercase or lowercase or a space?
+    // if a space, push space into temp holding array (if not a space, it's a letter)
+    if (str[i] === ' ') {
+      letters.push(str[i]);
+      // if already Upper Case, push lower Case version of letter into holding array
+    } else if (str[i] === str[i].toUpperCase()) {
+      letters.push(str[i].toLowerCase())
+      // if not space and not already Upper Case, push Upper Case version of letter to holding array
+    } else letters.push(str[i].toUpperCase())
+  }
+  // zip up the array of letters and spaces into a string
+  caseConvertedString = letters.join('');
+  return caseConvertedString;
+}
+
+const testCase1 = 'Fsdfsdf ESDFSDFdfsdfsdfsdfdsf';
+console.log(reverseCase(testCase1)); // ---> fSDFSDF esdfsdfDFSDFSDFSDFDSF
+
+// will it handle periods?
+const testCase2 = 'The quick brown Mr. Fox jumped over the lazy Mr. Dog.';
+console.log(reverseCase(testCase2)); // ---> tHE QUICK BROWN mR. fOX JUMPED OVER THE LAZY mR. dOG.
