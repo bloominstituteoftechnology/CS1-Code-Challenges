b4230965a38ee2cae03c2460ec428894a5c797a8
wlharvey4 <wlharvey4@mac.com>
2017-07-12 06:50:59 -0700

    isUnique.js

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -4,7 +4,15 @@
 */
 
 const isUnique = (str) => {
-
+  const set = new Set();
+  const check = (pos = 0) => {
+    if (pos === str.length) return true;
+    const s = str[pos];
+    if (set.has(s)) return false;
+    set.add(s);
+    return check(++pos);
+  }
+  return check();
 };
 
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
