f7cf95951bc30b2c791777e78810b3607ee18332
Ely Alamillo <el0013@gmail.com>
2017-07-12 21:49:00 -0500

    finished reverseCase

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -4,4 +4,27 @@
  * Assume that each string will contain only spaces and letters.
  */
 
+ const reverseCase = str => {
+  let i = 0;
+  const charArray = [];
+  while (i <  str.length) {
 
+    let char = str.charAt(i);
+    if (char === ' ') {
+      charArray.push(char);
+    }
+    else if (char === char.toUpperCase()) {
+      char = char.toLowerCase();
+      charArray.push(char);
+    }
+    else {
+    charArray.push(char.toUpperCase());
+    }
+    i++;
+  }
+  return charArray.join('');
+}
+
+console.log(reverseCase('Hello World')); // --> "hELLO wORLD"
+console.log(reverseCase('ely alamillo')); // --> "ELY ALAMILLO"
+console.log(reverseCase('ELY ALAMILLO')); // -->"ely alamillo"
