c201c9494a40c2d81c9e7c98b3fe4bd9ddbe469a
pat collins <padjo@pats-MBP-2.fritz.box>
2017-07-13 16:20:49 +0200

    reverseCase seems to be working

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -4,4 +4,28 @@
  * Assume that each string will contain only spaces and letters.
  */
 
+ // high level solution overview. Convert string to array so you can loop over it. Loop over it and
+ //each time check if the char === toUppderCase() if so convert it to lower case and add it to new array.
+ //convert new array to string and return it.
 
+ const stringReverse = str => {
+   //str to array
+   const arrayString = str.split('');
+   //console.log(arrayString);
+   resultStr = '';
+   for (var i = 0; i < arrayString.length; i++) {
+     // if the char is uppercase switch to lowecase and add to string otherwise toLowercase and add to string
+     if (arrayString[i] == arrayString[i].toUpperCase()) {
+       resultStr = resultStr + arrayString[i].toLowerCase();
+     } else {
+       resultStr = resultStr + arrayString[i].toUpperCase();
+     }
+   }
+   return (console.log(resultStr));
+   //return resultStr;
+
+ };
+
+ stringReverse('flaA');
+
+ // not exactly sure why i have to do a return (console log()); would have thought the return would have done ...
