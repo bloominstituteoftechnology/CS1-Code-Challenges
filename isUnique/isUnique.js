5c3e0d0cea791ff53ed242c185ad6baf900ef1a1
Derrick <clickspiker23@gmail.com>
2017-07-13 10:29:57 -0500

    reverse

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 /*
-=======
-/* 
->>>>>>> 7e76d6a009f694cbab0659f84fdabd517aac089b
 * Implement an algorithm to determine if a string has all unique characters.
 * Extra Credit - Answer this question - What if you cannot use additional data structures?
 */
 
 const isUnique = (str) => {
-<<<<<<< HEAD
   // i know technically it is the word being split but the variable name helps me break it down in my head
   let currentLetterToCompare = str.split("");
   for (let i = 0; i < str.length; i++) {
@@ -29,12 +24,7 @@ const isUnique = (str) => {
 return true;
 };
 
-console.log(isUnique('abcdhijklmnopqrstuv')); // true
-console.log(isUnique('abcdefga')); // false
-=======
 
-};
 
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
 console.log(isUnique('abcdefga')); // false
->>>>>>> 7e76d6a009f694cbab0659f84fdabd517aac089b
