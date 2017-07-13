adbb9125a810f0d9d0cafbb4f06af3da130066d1
Kayce Hubbard <kayce.hubbard@gmail.com>
2017-07-13 11:07:53 -0500

    Passes all tests.

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -4,4 +4,18 @@
  * Assume that each string will contain only spaces and letters.
  */
 
+const reverseCase = (str) => {
+  let newString = '';
+  for(let i = 0; i < str.length; i++) {
+  	let character = str.charCodeAt(i);
+    if((character >= 65) && (character <= 90)) {
+    	newString = newString + (str.charAt(i).toLowerCase());
+    } else if((str.charCodeAt(i) >= 97) && (str.charCodeAt(i) <= 122)) {
+    	newString = newString + (str.charAt(i).toUpperCase());
+    } else {
+    newString = newString + ' ';
+  }
+}
+  return newString;
+}
 
