85207665eb1de18f9880035fb9f4c71510014506
WizrdJim <wzrdjim@gmail.com>
2017-07-13 12:18:47 -0400

    incomplete

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -4,4 +4,26 @@
  * Assume that each string will contain only spaces and letters.
  */
 
+const isUpper = (ch) => {
+  const code = ch.charCodeAt(0);
+  if (code >=65 && code <= 90) return true;
+  return false;
+}
+const isLower = (ch) => {
+  const code = ch.charCodeAt(0);
+  if (code >=97 && code <= 122) return true;
+  return false;
+}
+const reverseCase = str => {
+    let newString = str;
+  for (let i = 0; i < str.length; i++) {
+    if (isUpper(str.charAt(i))) {
+      newString.charAt(i) = ToLowerCase(str.charAt(i));
+    } else if (isLower(str.charAt(i))){
+      newString.charAt(i) = ToUpperCase(str.charAt(i));
+    }
+  }
+  return newString;
+}
+console.log(reverseCase('foR'));
 
