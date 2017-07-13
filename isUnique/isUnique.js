9af8abba9c503b4ce06682492503476b3455e4a8
John Wells <JW@Johnnys-MacBook-Pro.local>
2017-07-13 01:00:19 -0700

    Finished Coding Challenge

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -18,5 +18,15 @@ const isUnique = (str) => {
   return 'this string is unique!';
 }
 
+// 2nd Solution using split
+// const isUnique = (str) => {
+//   const tester = str.split("").sort();
+//   let check = true;
+//   for (let i = 1; i < tester.length; i++) {
+//     if (tester[i] === tester[i-1]) check = false;
+//   }
+//   return check;
+// };
+
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
 console.log(isUnique('abcdefga')); // false
