b7cb8aa14e2c77fcfacded26265a964c8bfd00a4
mixelpixel <mixelpix@mac.com>
2017-07-14 01:28:05 -0400

    all palindrome test cases pass

--- largestPrimePalindrome/palindrome.js
+++ largestPrimePalindrome/palindrome.js
@@ -2,10 +2,17 @@ function isPalindrome(str) {
   // return true if string is a palindrome.
   // otherwise return false
   for (let i = 0; i < str.length / 2; i++) { // <--- make sure divided by 2 handles odd and even length (floor vs. ceiling)
-    if (str[i] === str[-1 - i]) {
-      retrun true;
+    if (str[i] === str[str.length - 1 - i]) {
+      return true;
     } else {
       return false;
     }
   }
 }
+
+console.log(`Q: Is 'palindrome' a palindrome?            A: ${isPalindrome('palindrome')}`) // <-- false
+console.log(`Q: Is ' ' a palindrome?                     A: ${isPalindrome(' ')}`) // <-- true
+console.log(`Q: Is 'a' a palindrome?                     A: ${isPalindrome('a')}`) // <-- true
+console.log(`Q: Is 'bb' a palindrome?                    A: ${isPalindrome('bb')}`) // <-- true
+console.log(`Q: Is 'cdc' a palindrome?                   A: ${isPalindrome('cdc')}`) // <-- true
+console.log(`Q: Is 'amanaplanacanalpanama' a palindrome? A: ${isPalindrome('amanaplanacanalpanama')}`) // <-- true
