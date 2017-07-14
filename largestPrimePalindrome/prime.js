7cd0651bec79062f95f52933e5739accd00bada3
mixelpixel <mixelpix@mac.com>
2017-07-14 01:30:48 -0400

    prime time

--- largestPrimePalindrome/prime.js
+++ largestPrimePalindrome/prime.js
@@ -14,6 +14,7 @@ const isPrime = function(num) {
   } return true;
 }
 
+// PRIME TEST SUITE
 // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
 console.log(`Q:      0 is prime? A: ${isPrime(0)}`)     // <--- false
 console.log(`Q:      1 is prime? A: ${isPrime(1)}`)     // <--- false
