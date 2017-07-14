e0b4d3b4214493af4922fd11fb9f7ac6a476a649
pat collins <padjo@pats-MBP-2.fritz.box>
2017-07-15 01:21:28 +0200

    got the function working with the help of Tai

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -6,3 +6,44 @@
  * Extra credit: Modify the function to look for the largest prime palindrome less than any provided number.
  * Extra credit 2: How can you improve the time complexity?  (Google: Sieve of Eratosthenes)
  */
+
+ // general outline of a solution convert the string to an array to make it easier to loop over and use array
+ // functions .  loop over the string starting at num 1000 and decrementing
+ //use array.reverse to reverse the array and then array.join to convert it again to a string. compare the resulting
+ //string to see if they are equal to the loop value, if so it's a palindrome , set palindrome to true.
+ // now check to see if it's a prime number . if it is
+//  return the number other wise go down checking to zero
+ //and if nothing is found return 'nothing found'
+
+
+ const largestPrimePlaindrome = num => {
+    for (let i=num; i > 0; i--)  {
+     const arrStr = i.toString().split('');
+     //reverse the string and check if they are the same (palindromes)
+     const reversedArr = arrStr.slice().reverse(); //.slce() creates a new array
+     // now you have the arrNumber and reversedArr check to see if they are equal
+     let isPalindrome = true;
+     let isPrimeNum = true;
+     // check for palindrome
+     arrStr.forEach((char, i) => {
+       if (arrStr[i] !== reversedArr[i]) {
+          isPalindrome = false;
+       }
+     });
+     //
+     let divisor = i - 1;
+     while(divisor > 1) {
+       if (i % divisor === 0 ) {
+        isPrimeNum = false;
+       }
+      divisor--;
+     }
+     
+     if (isPrimeNum && isPalindrome) {
+       return i;
+     }
+   }
+
+ };
+
+ console.log(largestPrimePlaindrome(930));
