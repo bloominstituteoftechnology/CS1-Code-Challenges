5625bd34669b89987e67f2ab95bd2a00d030e24d
mixelpixel <mixelpix@mac.com>
2017-07-14 00:11:32 -0400

    isPrime

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -6,3 +6,33 @@
  * Extra credit: Modify the function to look for the largest prime palindrome less than any provided number.
  * Extra credit 2: How can you improve the time complexity?  (Google: Sieve of Eratosthenes)
  */
+
+ function isPrime(num) {
+   //return true if num is prime.
+   //otherwise return false
+   //hint: a prime number is only evenly divisible by itself and 1
+   //hint2: you can solve this using a for loop
+   //note: 0 and 1 are NOT considered prime numbers
+
+   // if (num < 2) {return false;}
+   // else if (num === 2) {return true;}
+   // else if (num % 2 === 0) {return false;}
+   // else if (num >= 3) {
+   //   for (var i = 2; i < (num * .5); i++) {
+   //     if (num % i === 0) {
+   //       return false;
+   //     }
+   //   }
+   // } return true;
+
+   if (num < 2) {
+     return false;
+   }
+   for (var i = 2; i <= num; i++) {
+     if ( num % i == 0 && i != num) {
+       return false;
+     } else {
+       return true;
+     }
+   }
+}
