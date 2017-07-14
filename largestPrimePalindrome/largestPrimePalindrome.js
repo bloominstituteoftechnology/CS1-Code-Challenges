229afb887279a52dfa01c62c9d0c39a1a4719db3
mixelpixel <mixelpix@mac.com>
2017-07-14 00:34:50 -0400

    isPalindrome to do: test odd vs even str.length

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -7,32 +7,42 @@
  * Extra credit 2: How can you improve the time complexity?  (Google: Sieve of Eratosthenes)
  */
 
- function isPrime(num) {
-   //return true if num is prime.
-   //otherwise return false
-   //hint: a prime number is only evenly divisible by itself and 1
-   //hint2: you can solve this using a for loop
-   //note: 0 and 1 are NOT considered prime numbers
+function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) { // <--- make sure divided by 2 handles odd and even length (floor vs. ceiling)
+    if (str[i] === str[-1 - i]) {
+      retrun true;
+    } else {
+      return false;
+    }
+  }
+}
 
-   // if (num < 2) {return false;}
-   // else if (num === 2) {return true;}
-   // else if (num % 2 === 0) {return false;}
-   // else if (num >= 3) {
-   //   for (var i = 2; i < (num * .5); i++) {
-   //     if (num % i === 0) {
-   //       return false;
-   //     }
-   //   }
-   // } return true;
+function isPrime(num) {
+  //return true if num is prime.
+  //otherwise return false
+  //hint: a prime number is only evenly divisible by itself and 1
+  //hint2: you can solve this using a for loop
+  //note: 0 and 1 are NOT considered prime numbers
 
-   if (num < 2) {
-     return false;
-   }
-   for (var i = 2; i <= num; i++) {
-     if ( num % i == 0 && i != num) {
-       return false;
-     } else {
-       return true;
-     }
-   }
+  // if (num < 2) {return false;}
+  // else if (num === 2) {return true;}
+  // else if (num % 2 === 0) {return false;}
+  // else if (num >= 3) {
+  //   for (var i = 2; i < (num * .5); i++) {
+  //     if (num % i === 0) {
+  //       return false;
+  //     }
+  //   }
+  // } return true;
+
+  if (num < 2) {
+    return false;
+  }
+  for (var i = 2; i <= num; i++) {
+    if ( num % i == 0 && i != num) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
