8ff940bdc673cfba3c2a8cc0d43c98a114abb9f0
Latoyya Smith <latoyyas@gmail.com>
2017-07-14 01:36:44 -0500

    completed exercise

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -6,3 +6,26 @@
  * Extra credit: Modify the function to look for the largest prime palindrome less than any provided number.
  * Extra credit 2: How can you improve the time complexity?  (Google: Sieve of Eratosthenes)
  */
+
+const largestPrimePalindrome = () => {
+  
+  const isPalindrome = num => {
+      return num == num.toString().split('').reverse().join('');
+   };
+  
+  const isPrime = num => {
+    for(let i = 2; i < num; i++) {
+      if(num % i === 0) {
+        return false;
+      }
+    }
+    return num !== 1;
+  };
+  
+  for (let i = 920; i < 1000; i++) {
+    if (isPalindrome(i) && isPrime(i)) {
+      return i;
+    } 
+  }
+}
+console.log(largestPrimePalindrome());
