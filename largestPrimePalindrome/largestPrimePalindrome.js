ce8bf6442d81435c7506e2fea539576325dbf40b
mixelpixel <mixelpix@mac.com>
2017-07-14 01:31:09 -0400

    added test suite to verify

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -34,3 +34,28 @@ function isPalindrome(str) {
     }
   }
 }
+// PALINDROME TEST SUITE
+console.log(`Q: Is 'palindrome' a palindrome?            A: ${isPalindrome('palindrome')}`) // <-- false
+console.log(`Q: Is ' ' a palindrome?                     A: ${isPalindrome(' ')}`) // <-- true
+console.log(`Q: Is 'a' a palindrome?                     A: ${isPalindrome('a')}`) // <-- true
+console.log(`Q: Is 'bb' a palindrome?                    A: ${isPalindrome('bb')}`) // <-- true
+console.log(`Q: Is 'cdc' a palindrome?                   A: ${isPalindrome('cdc')}`) // <-- true
+console.log(`Q: Is 'amanaplanacanalpanama' a palindrome? A: ${isPalindrome('amanaplanacanalpanama')}`) // <-- true
+// PRIME TEST SUITE
+// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
+console.log(`Q:      0 is prime? A: ${isPrime(0)}`)     // <--- false
+console.log(`Q:      1 is prime? A: ${isPrime(1)}`)     // <--- false
+console.log(`Q:      2 is prime? A: ${isPrime(2)}`)     // <--- true
+console.log(`Q:      3 is prime? A: ${isPrime(3)}`)     // <--- true
+console.log(`Q:      4 is prime? A: ${isPrime(4)}`)     // <--- false
+console.log(`Q:      5 is prime? A: ${isPrime(5)}`)     // <--- true
+console.log(`Q:      6 is prime? A: ${isPrime(6)}`)     // <--- false
+console.log(`Q:      7 is prime? A: ${isPrime(7)}`)     // <--- true
+console.log(`Q:      8 is prime? A: ${isPrime(8)}`)     // <--- false
+console.log(`Q:      9 is prime? A: ${isPrime(9)}`)     // <--- false
+console.log(`Q:     10 is prime? A: ${isPrime(10)}`)    // <--- false
+console.log(`Q:     11 is prime? A: ${isPrime(11)}`)    // <--- true
+console.log(`Q:     12 is prime? A: ${isPrime(12)}`)    // <--- false
+console.log(`Q:     13 is prime? A: ${isPrime(13)}`)    // <--- true
+console.log(`Q:    121 is prime? A: ${isPrime(121)}`)   // <--- false (11 * 11 = 121)
+console.log(`Q: 104729 is prime? A: ${isPrime(104729)}`)// <--- true (104,729 is the 10,000th prime number!)
