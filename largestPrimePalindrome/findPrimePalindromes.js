9a3fe0f24e042a7aa1f8efa459fcb4091b620a08
mixelpixel <mixelpix@mac.com>
2017-07-15 00:12:09 -0400

    findPrimePalindromes √

--- largestPrimePalindrome/findPrimePalindromes.js
+++ largestPrimePalindrome/findPrimePalindromes.js
@@ -66,10 +66,12 @@ const numToStr = function(num) {
 // console.log(`Q: Is 'cdc' a palindrome?                   A: ${isPalindrome('cdc')}`) // <-- true
 // console.log(`Q: Is 'amanaplanacanalpanama' a palindrome? A: ${isPalindrome('amanaplanacanalpanama')}`) // <-- true
 // console.log(`Q: Is an integer a palindrome?              A: ${isPalindrome(5)}`) // <-- undefined
+
 // // numToStr() test
 // console.log(numToStr(5)); // <--- '5'
 // console.log(typeof numToStr(5)); // <--- string
 // console.log(`Q: Is the integer converted to a string?    A: ${isPalindrome(numToStr(5))}`) // <-- true
+
 // // isPrime TEST SUITE
 // // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
 // console.log(`Q: Is   0    prime? A: ${isPrime(0)}`)     // <--- false
@@ -88,12 +90,15 @@ const numToStr = function(num) {
 // console.log(`Q: Is   13   prime? A: ${isPrime(13)}`)    // <--- true
 // console.log(`Q: Is  121   prime? A: ${isPrime(121)}`)   // <--- false (11 * 11 = 121)
 // console.log(`Q: Is 104729 prime? A: ${isPrime(104729)}`)// <--- true (104,729 is the 10,000th prime number!)
+
 // // findPrimes TEST SUITE
 // [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127]
 console.log(`Q: what are the primes from 0 to 127?\nA: ${findPrimes(127)}`);
 // console.log(`Q: what are the primes from 0 to 121?\nA: ${findPrimes(104729)}`); // <--- 10,000 prime numbers!!!!
 console.log(findPrimes(104729).length);
+
 // findPrimePals TEST SUITE
 // [11, 101, ...]
-console.log(`Q: what are the prime palindromes from 0 to 127?\nA: ${typeof findPrimePals(findPrimes(127))}`);
-console.log(`Q: what are the prime palindromes from 0 to 127?\nA: ${findPrimePals(findPrimes(127))}`);
+console.log(`Q: Is findPrimePals(findPrimes(127)) an instance of an array?\nA: ${findPrimePals(findPrimes(127)) instanceof Array}`); // <--- true
+console.log(`Q: what are the prime palindromes from 0 to 127?\nA: ${findPrimePals(findPrimes(127))}`); // <--- 2,3,5,7,11,101
+console.log(`Q: what are the prime palindromes from 0 to 1000?\nA: ${findPrimePals(findPrimes(1000))}`); // <--- 2,3,5,7,11,101,131,151,181,191,313,353,373,383,727,757,787,797,919,929
