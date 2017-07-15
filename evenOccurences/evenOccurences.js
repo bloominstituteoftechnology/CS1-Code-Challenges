7e29a739d9567bf15106975e08566fadd9f3c1bb
mixelpixel <mixelpix@mac.com>
2017-07-15 17:49:47 -0400

    countNotUnique passes test suite √

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -13,30 +13,47 @@
 // O: array item - first instance of item which occurs even # of times in array
 const evenOccurence = (arr) => {
 
-  const isUnique = (str) => {
+  // I: array
+  // O: if (isUnique(array)) ? uniqueArrayItem : null
+  const isUnique = (str) => { // <----- works fine for arrays
     for (let i = 0; i < str.length; i++) {
       for (let j = i + 1; j < str.length; j++) {
         // console.log(`Q: does ${str[i]} === ${str[j]}? A: ${str[i] === str[j]}`)
         if (str[i] === str[j]) {
-          return false;
+          return str[i];
         }
       }
-    }  return true;
+    }  return null;
   };
-  // isUnique TEST SUITE:
-  console.log(isUnique('abcdhijklmnopqrstuv')); // true
-  console.log(isUnique('abcdefghijklmnopqrstuvwyz')); // true
-  console.log(isUnique('abcdefga')); // false
-  console.log(isUnique('bcdgefga')); // false
-  console.log(isUnique([1, 2, 3, 4, 5, 6, 3])); // false
-  console.log(isUnique([0, 1, 2, 3, 4, 6, 7, 8, 9, 9])); // false
+// // isUnique TEST SUITE:
+// console.log(isUnique('abcdhijklmnopqrstuv')); // true
+// console.log(isUnique('abcdefghijklmnopqrstuvwyz')); // true
+// console.log(isUnique('abcdefga')); // false
+// console.log(isUnique('bcdgefga')); // false
+// console.log(isUnique([1, 2, 3, 4, 5, 6, 3])); // false
+// console.log(isUnique([0, 1, 2, 3, 4, 6, 7, 8, 9, 9])); // false
 
-
-  const notUnique = () => {
-
+  // I: array
+  // O: 1st item that occurs an even number of times in an array
+  const countNotUnique = (item, anArrayOfItems) => {
+    const hold  = [];
+    if (!isUnique(item)) {
+      for (let i = 0; i < anArrayOfItems.length; i++) {
+        if (anArrayOfItems[i] === item) {
+          hold.push(item);
+        }
+      }
+    }
+    const count = hold.length;
+    if ((count === 0) || (count % 2 !== 0)) {
+      console.log(`no dupes || odd # of dupes`);
+    }
+    console.log(`${anArrayOfItems} has ${count} many instances of ${item}: ${hold}`)
   }
-
-  // return isUnique(arr)
+// countNotUnique TEST SUITE:
+countNotUnique(1, [5, 1, 1, 1, 1]); // <---------------------- 4 instances of 1
+countNotUnique(1, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); // <--- no dupes >>> null
+countNotUnique(1, [5, 1, 1, 1, 6]); // <---------------------- odd dupes >> null
 
 };
 
