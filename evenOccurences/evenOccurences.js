3c4be3faa4f0a31c883bf4b29b85e48807289696
mixelpixel <mixelpix@mac.com>
2017-07-15 21:32:06 -0400

    version 1 comsoles removed

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -7,23 +7,37 @@
   * console.log(onlyEven); //  4
  */
 
+// // version 2 √
+//  const evenOccurence = (arr) => {
+//    const unique = [...new Set(arr)]; // <---------- Will Set maintain list order?????
+//    let count = 0;
+//    for (i in unique) {
+//      count = 0;
+//      for (j in arr) {
+//        if (unique[i] === arr[j]) {
+//          count++;
+//        }
+//      }
+//      // if (count % 2 === 0) console.log(`${unique[i]} occurs ${count} times.`);
+//      if (count % 2 === 0) return unique[i]; // <--- how to write this as a ternary operator?
+//      // return (count % 2 === 0) ? unique[i] : null;
+//    }
+//  };
+
+// VERSION 1 √
 // I: array
 // F(): if isUnique is false, then return null, then check total number of times
 //      item is in array, %2 === 0 dun, or check next until null
 // O: array item - first instance of item which occurs even # of times in array
 const evenOccurence = (arr) => {
   arr;
-  // console.log(arr);
 
   // I: array
   // O: if (isUnique(anArray)) ? 1st unique array item : null
   const isUnique = (anArray) => {
-    if (anArray === null) /* console.log(`That's some kinda ${anArray} isUnique array there`); */ return null;
-    // console.log(anArray);
-    // console.log(`${anArray} ...next stop is the for loop:`);
+    if (anArray === null) return null;
     for (let i = 0; i < anArray.length; i++) {
       for (let j = i + 1; j < anArray.length; j++) {
-        // console.log(`Q: does ${anArray[i]} === ${anArray[j]}? A: ${anArray[i] === anArray[j]}`)
         if (anArray[i] === anArray[j]) {
           return anArray[i];
         }
@@ -49,29 +63,22 @@ const evenOccurence = (arr) => {
 
   const oddOrEven = (dupesArray) => {
     const minusOddDupe = [];
-    if (dupesArray === null) /* console.log(`That's some kinda ${dupesArray} oddOrEven array`); */ return null;
+    if (dupesArray === null) return null;
     const count = dupesArray.length;
-    // console.log(count);
     if (count === 0) {
-      /* console.log(`no dupes || odd # of dupes`); */return null;
+      return null;
     } else if (count % 2 !== 0) {
-      // console.log(`that's odd: ${arr}`)
       for (let i = 0; i < arr.length; i++) {
-        // console.log(`if ${dupesArray[0]} !== ${arr[i]}`);
         if (dupesArray[0] !== arr[i]) {
           minusOddDupe.push(arr[i]);
         }
-        // console.log(`minus dupes? ${minusOddDupe}`);
       }
       return evenOccurence(minusOddDupe);
     }
-    // console.log(`The holding array for duplicates has ${count} instances of ${dupesArray[0]}: ${dupesArray}`)
     return dupesArray[0];
-    // return dupesArray.length;
   }
 
   return oddOrEven(countNotUnique(isUnique(arr), arr));
-
 };
 
 // evenOccurence TEST SUITE:
@@ -82,7 +89,6 @@ const evenOccurence = (arr) => {
 // evenOccurence([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); // <----- null
 // evenOccurence([5, 1, 1, 1, 6, 1, 1]); // <--------- 1 x5 >>> null
 // evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]); // <--- 4 x2 >>> 4
-
 
 const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
 console.log(onlyEven);
