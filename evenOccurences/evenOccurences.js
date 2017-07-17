b1527a2c64fd8e26be1c1d8dcf8cb442994a9776
Ely Alamillo <el0013@gmail.com>
2017-07-16 19:23:29 -0500

    finished evenOccurences

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -10,6 +10,23 @@
  *    * console.log(onlyEven); //  4
  *    */
 
-const evenOccurence = (arr) => {
-  // Your code here.
-};
+ const evenOccurence = (arr) => {
+   // Your code here.
+   let numberStorage = {};
+   arr.forEach((number) => {
+     if (numberStorage[number]) {
+       numberStorage[number] = numberStorage[number]+1
+     } else {
+       numberStorage[number] = 1;
+     }
+   })
+   for (let i = 0; i < arr.length; i++) {
+     let currentNumber = arr[i];
+     if (numberStorage[currentNumber] % 2 === 0) {
+       return currentNumber;
+     }
+   }
+   return null;
+ };
+ const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
+ console.log(onlyEven);
