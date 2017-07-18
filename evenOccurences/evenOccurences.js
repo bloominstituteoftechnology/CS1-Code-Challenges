1ecc3017e5c05390ebb3e58465ba7184370ec5f9
pat collins <padjo@pats-MacBook-Pro-2.local>
2017-07-18 07:57:32 +0100

    got it outputting something but not correct

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -10,6 +10,26 @@
  *    * console.log(onlyEven); //  4
  *    */
 
+  //
+
 const evenOccurence = (arr) => {
   // Your code here.
+
+  arr.forEach((elem, i) => {
+    let result = 0;
+    while ((arr.indexOf(elem, i + 1)) !== -1){
+        result ++;
+        ++i;
+        console.log(`pat ${elem} ${i}`);  //this is where my bug is
+    }
+    console.log(`the result is ${result}`);
+    if (result > 0 && result % 2 == 0) {
+      return  elem;
+    }
+  });
+  // no even matches found return null
+  return null;
 };
+
+const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
+console.log(`finala finala result ${onlyEven}`);
