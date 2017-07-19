ff126db367974bf82c776edaa05e8a2b07a94fb5
manishavi <manishalal972@gmail.com>
2017-07-19 12:02:18 -0500

    solved

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -12,4 +12,22 @@
 
 const binarySearch = (nums, target) => {
 
+	let min = 0;
+    let max = nums.length - 1;
+
+    while (min <= max){
+
+       mid = Math.floor((min + max)/2);
+
+    	if(nums[mid] === target) {
+    		return mid;
+    	}
+        else if (nums[mid] < target){
+      	    min = mid + 1;
+        }
+        else if (nums[mid] > target){
+    	    max = mid - 1;
+        }
+    }
+
 };
