172eb58f87874fdc4f565b934ea463bb7442cbba
pat collins <padjo@pats-MacBook-Pro-2.local>
2017-07-20 16:56:45 +0100

    code is not mine unfortunately

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -10,6 +10,31 @@
  *	* console.log(index); // 1
 **/
 
-const binarySearch = (nums, target) => {
 
-};
+/* EVERYTHING stolen from here ,just wanted to submit a working something
+https://www.nczonline.net/blog/2009/09/01/computer-science-in-javascript-binary-search/ */
+function binarySearch(items, value){
+
+    var startIndex  = 0,
+        stopIndex   = items.length - 1,
+        middle      = Math.floor((stopIndex + startIndex)/2);
+
+    while(items[middle] != value && startIndex < stopIndex){
+
+        //adjust search area
+        if (value < items[middle]){
+            stopIndex = middle - 1;
+        } else if (value > items[middle]){
+            startIndex = middle + 1;
+        }
+
+        //recalculate middle
+        middle = Math.floor((stopIndex + startIndex)/2);
+    }
+
+    //make sure it's the right value
+    return (items[middle] != value) ? -1 : middle;
+}
+
+const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+console.log(index);
