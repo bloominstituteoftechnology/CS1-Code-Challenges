386f3d4ce6132f50c0d6e839b255072f07eba5ce
pat collins <padjo@pats-MacBook-Pro-2.local>
2017-07-21 17:35:55 +0100

    think i solved it, non programatically

---
+++ brainTeasers/waterJugs.js
@@ -0,0 +1,9 @@
+You have a five-quart jug, a three-quart jug, and an unlimited supply of water (but no measuring cups). How would you come up with exactly four quarts of water? Note that the jugs are oddly shaped, such that filling up exactly "half" of the jug would be impossible.
+
+
+put 3 into the 5 quart jug
+put 2 into the 5 quart jug ie until the 5 quarter is full
+you have one left in the 3 quart jug, empty the 5 quart jug,
+put in the one quart from the 3 ,
+fill the 3 dump in the 5 quart.
+you now have 4 quarts of water in the 5 quart jug, prost. 
