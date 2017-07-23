9a1ba4cbd57d868c0cf75e944210e7e24f95aa18
sfmajors373 <sfmajors373@gmail.com>
2017-07-23 20:16:10 +0000

    Array: tests added, not passing

--- array/array.js
+++ array/array.js
@@ -7,3 +7,73 @@
  * How do these operations compare to that of a linked list?
  * How does the time complexity of insertion and deletion compare to that of a linked list?
  */
+class Array {
+  constructor(item) {
+    this[0] = item;
+  }
+
+  length(){
+    let counter = 0;	  
+    for (key in this) {
+      counter++;
+    }
+    return counter;
+  }
+
+  push(item){
+    this[this.length] = item;
+  }
+
+  pop(// I know you are going to try to put something in these parentheses - Don't do it
+    ){
+    for (key in this) {
+      if (this.key === this.length) {
+        delete this.key;
+      }
+    }
+  }
+
+  get(index){
+    for (key in this) {
+      if (this.key === index) {
+        return this.value;
+      }
+    }
+  }
+
+  delete(index){
+    // make sure to shift the items after deletion
+  }
+
+}
+
+//**************************************************************************************************
+// Check if constructor works
+// const arr = new Array('first');
+// console.log(arr);
+// Check if push works (builds on constructor working test)
+// arr.push('second');
+// arr.push('third');
+// arr.push('fourth');
+// console.log(arr);
+// ***********************Leave this arr for remaining tests
+// check if length works
+const arr = {
+  0: 'first',
+  1: 'second',
+  2: 'third',
+  3: 'fourth',
+}
+// *****************************************
+console.log(arr.length); // 4
+// check if push works 
+// arr.push('fifth');
+// console.log(arr);
+// check if pop works
+// arr.pop();
+// console.log(arr);
+// test get(index)
+// console.log(arr.get(2)); // third
+// test delete 
+// arr.delete(2);
+// console.log(arr);
