8b5daab1a40054d1d0c84a2d14e58053a0965949
jeshsy <jeshsy@users.noreply.github.com>
2017-07-24 12:45:33 -0400

    got string compression to work

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,19 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+const compress = (str) => {
+  let counter = 1;
+  let newstr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (i === str.length - 1) newstr = newstr + str[i] + counter; 
+    else if (str[i + 1] === str[i]) counter += 1;
+    else {
+    newstr = newstr + str[i] + counter;
+    counter = 1;
+    }
+  }
+  if (newstr.length < str.length) return newstr;
+}
+
+console.log(compress('aabcccccaaa'));
