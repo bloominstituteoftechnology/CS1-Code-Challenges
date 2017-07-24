3e68152be341743cf01421848fd01d8675132bad
manishavi <manishalal972@gmail.com>
2017-07-24 11:33:10 -0500

    array done

--- array/array.js
+++ array/array.js
@@ -7,3 +7,33 @@
  * How do these operations compare to that of a linked list?
  * How does the time complexity of insertion and deletion compare to that of a linked list?
  */
+ class Array {
+   constructor() {
+     const array = [];
+   }
+
+   push() {
+     return this.index++;
+   }
+   pop() {
+     return this.index--;
+   }
+   get(index) {
+     return this.array[index];
+   }
+   delete(index) {
+     if (this.index === index) {
+       this.index = this.index - 1;
+     }
+     return this.array.shift();
+   }
+ }
+
+ const array = new Array;
+ array.push(1);
+ array.push(3);
+ array.push(5);
+ array.push(7);
+ console.log(array);
+ console.log(array.get(2));
+ console.log(array.delete(0));
