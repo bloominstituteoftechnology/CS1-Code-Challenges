33b2a338bd259df03d244a59f15e03ad7254acae
Ely Alamillo <el0013@gmail.com>
2017-07-24 21:26:28 -0500

    finished stringCompression

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,26 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+const strCompression = (string) => {
+  const strMap = {};
+  const strArray = str.split('')
+  strArray.forEach((letter) => {
+    if (strMap[letter]) {
+      strMap[letter] += 1;
+    } else {
+      strMap[letter] = 1;
+    }
+  });
+
+  const newString = JSON.stringify(Object.entries(strMap));
+  const re = /\W/g
+  const rem = newString.replace(re, '');
+
+  if (rem.length < string) return rem;
+  return string;
+}
+const str = 'aaabbbccc'
+const str1 = 'aabbcc'
+console.log(strCompression(str));
+console.log(strCompression(str1));
