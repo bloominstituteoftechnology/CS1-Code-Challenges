a17ec8a114370874fb0a1f31ac1d07b7475a6b82
Lois Truby <loyslane@gmail.com>
2017-07-25 12:44:14 -0400

    completed operators challenge

--- operators/operators.js
+++ operators/operators.js
@@ -4,3 +4,33 @@
  * Now for the tricky part: you can only use the + and - operators to implement these functions.
  * For division just drop the remainder.
  */
+
+const multiply = (x, y) => {
+	let product = x;
+	if (x > y) {
+		for (i = 1; i < y; i++)
+			product += x;
+	} else {
+		for (i = 1; i < y; i++)
+			product += x;
+	}
+	return product;
+};
+
+const divide = (x, y) => {
+	let quotient = 0;
+	let runningTotal = x;
+	while (runningTotal - y >= 0) {
+		runningTotal -= y;
+		quotient++;
+	}
+	return quotient;
+};
+
+const modulo = (x, y) => {
+	let remainder = x;
+	while (remainder - y >= 0) {
+		remainder -=y;
+	}
+	return remainder;
+};
