5f1308be62f2c1996957e7ad7a3317572e6ac48a
sfmajors373 <sfmajors373@gmail.com>
2017-07-25 10:40:54 -0400

    string compression: tests pass

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -6,30 +6,26 @@
 // You can assume the string has only uppercase and lowercase letters (a - z).
 
 function stringCompression(string) {
-const arr = [];
-let counter = 0;
-for (let i = 0; i < string.length; i++) {
-  if (string[i] === string[i + 1]) {
-    counter++;
-  } else {
-    arr.push(string[i]);
-    arr.push(counter);
-    counter = 0;
-  }
-}
-// check for end of string
-  // if not end of string
-  // else
-    // check if new string will be shorter than original
-    if (arr.concat('').length < string.length) {
-      return arr.concat('').length;
+  const arr = [];
+  let counter = 1;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[(i + 1)]) {
+      counter++;
+    } else {
+      arr.push(string[i]);
+      arr.push(counter);
+      counter = 1;
     }
-    return string;
+  }
+  if (arr.concat('').length < string.length) {
+    return arr.join('');
+  }
+  return string;
 };
 
 
 
 // ******************Tests***************
 console.log(stringCompression('aabcccccaaa'));  //a2b1c5a3
-// console.log(stringCompression('abcd'));  //abcd
-// console.log(stringCompression('aAbBccccDDDD')); //aAbBccccDDDD
+console.log(stringCompression('abcd'));  //abcd
+console.log(stringCompression('aAbBccccDDDD')); //aAbBccccDDDD
