4c9d771b44c5b8594152e3dfc69dfa2b3171a8f1
mixelpixel <mixelpix@mac.com>
2017-07-25 11:17:16 -0400

    basic compression, no bloat stop

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -6,37 +6,27 @@
 // You can assume the string has only uppercase and lowercase letters (a - z).
 
 
-// const compress = function(str) {
-//   // ...
-// }
 
-class Compressor {
-  constructor(upperOrLowerCaseString) {
-    this.string = upperOrLowerCaseString;
-    this.count = 1;
-    this.arr = [];
-    this.worthCompressing = (compareStringLength ? true : false)
-  }
 
-  totalRepetitions() {
-    for (let i = 0; i < this.string.length; i++) {
-      if (this.string.charAt(i) === this.string.charAt(i + 1)) { this.count++; }
+const compressor = function(str) {
+  // halt if compressing would make a longer string
+
+  // compression algorithm
+  const arr = [];
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else if ((str[i] != str[i + 1])) {
+      arr.push(`${str[i]}${count}`);
+      count = 1
     }
-    return this.count
   }
-
-  compareStringLength(str) {
-    // if there are no letters repeated more than twice, not worth compressing
-  }
-
-}
-
-
+  return arr;
+};
 
 
 // TEST SUITE
-const test = new Compressor('aabcccccaaa');
-// const test = new Compressor('abca');
+const test = compressor('aabcccccaaa'); // ---> a2b1c5a3
 console.log(test);
-console.log(test.totalRepetitions()); // ---> 8
-console.log(test.compareStringLength()); // ---> a2b1c5a3
+console.log(compressor('abca'));        // ---> a1b1c1a1
