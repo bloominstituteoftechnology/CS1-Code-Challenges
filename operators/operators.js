9040d7bed6e3688ac53bae0c0b0990f8a01b8c67
sfmajors373 <sfmajors373@gmail.com>
2017-07-25 12:07:40 -0400

    operators: tests bench created

--- operators/operators.js
+++ operators/operators.js
@@ -4,3 +4,20 @@
  * Now for the tricky part: you can only use the + and - operators to implement these functions.
  * For division just drop the remainder.
  */
+function multiply(){
+}
+
+function divide(){
+}
+
+function modulo(){
+}
+
+// *************Tests*******************
+// console.log(multiple(7,5)); // 35
+// console.log(divide(35,7)); // 5
+// console.log(divide(37,7)); // 5
+// console.log(divide(37,0)); // undefined
+// console.log(modulo(37,7)); // 2
+// console.log(modulo(35,5)); // 0
+// console.log(modulo(35,0)); // undefined
