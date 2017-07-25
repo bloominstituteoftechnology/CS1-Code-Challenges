a8ab1a5f5c160278931251d42c125b2cfa271a33
sfmajors373 <sfmajors373@gmail.com>
2017-07-25 12:34:18 -0400

    operators: tests pass

--- operators/operators.js
+++ operators/operators.js
@@ -4,20 +4,55 @@
  * Now for the tricky part: you can only use the + and - operators to implement these functions.
  * For division just drop the remainder.
  */
-function multiply(){
+function multiply(x,y){
+	let acc = 0;
+	if (x === 0 || y === 0) {
+	  return 0;
+	}
+  for (let i = 0;i < y; i++) {
+	  acc = acc + x;
+	}
+  return acc;
 }
 
-function divide(){
+function divide(x,y){
+  let acc = x;
+	counter = 0;
+	if (y === 0) {
+	  return undefined;
+	} else if (x === 0) {
+	  return 0;
+	}
+	while (acc >= y) {
+	  acc = acc - y;
+		counter ++;
+	}
+	return counter;
 }
 
-function modulo(){
+function modulo(x,y){
+  let acc = x;
+	counter = 0;
+	if (y === 0) {
+	  return undefined;
+	} else if (x === 0) {
+	  return 0;
+	}
+	while (acc >= y) {
+	  acc = acc - y;
+		counter ++;
+	}
+	return acc;
+  
 }
 
 // *************Tests*******************
-// console.log(multiple(7,5)); // 35
+// console.log(multiply(7,5)); // 35
+// console.log(multiply(7,0)); // 0
+// console.log(multiply(0,0)); // 0
 // console.log(divide(35,7)); // 5
 // console.log(divide(37,7)); // 5
 // console.log(divide(37,0)); // undefined
-// console.log(modulo(37,7)); // 2
-// console.log(modulo(35,5)); // 0
-// console.log(modulo(35,0)); // undefined
+console.log(modulo(37,7)); // 2
+console.log(modulo(35,5)); // 0
+console.log(modulo(35,0)); // undefined
