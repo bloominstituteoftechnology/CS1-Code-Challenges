d5b928877045356a0bb0ce5baa2a7ee492152090
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-26 05:29:28 -0400

    all done except stringCompression and largestPrimePalindrome

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,15 +1,15 @@
-/*
- * Given a sorted array, find the index of the specified element
- * using binary search.
- * https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
- * https://en.wikipedia.org/wiki/Binary_search_algorithm
- * */
-
-/**
- *  * const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
- *	* console.log(index); // 1
-**/
-
 const binarySearch = (nums, target) => {
-
+  let min = 0;
+  let max = nums.length - 1;
+  let guess;
+  while(max > min) {
+    guess = Math.floor((min + max) / 2);
+    if (guess === target) return nums.indexOf(guess);
+    else if (guess < target) min = guess + 1;
+    else max = guess - 1;
+  }
+  return -1;
 };
+
+const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+index;
--- brainTeasers/waterJugs.md
+++ brainTeasers/waterJugs.md
@@ -1,3 +1,37 @@
 You have a five-quart jug, a three-quart jug, and an unlimited supply of water (but no measuring cups).
 How would you come up with exactly four quarts of water?
 Note that the jugs are oddly shaped, such that filling up exactly "half" of the jug would be impossible.
+
+a = fiveQuartJug
+b = threeQuartJug
+
+fill b
+b = 3
+
+pour b into a
+a = 3
+b = 0
+
+fill b
+a = 3
+b = 3
+
+pour b into a util a is full
+a = 5
+b = 1
+
+dump a
+a = 0
+b = 1
+
+pour a into b
+a = 1
+b = 0
+
+fill b
+a = 1
+b = 3
+
+pour b into a
+a = 4
+b = 0
