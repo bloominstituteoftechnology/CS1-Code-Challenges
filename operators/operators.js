a9381e977aaf1183b473d927f0a82e401c8a938e
wlharvey4 <pinecone062@gmail.com>
2017-07-25 23:34:54 -0700

    operators.js completed

--- operators/operators.js
+++ operators/operators.js
@@ -4,3 +4,19 @@
  * Now for the tricky part: you can only use the + and - operators to implement these functions.
  * For division just drop the remainder.
  */
+
+const multiply = (x, y) => {
+  if (!y) return 0;
+  return multiply(x, y - 1) + x;
+}
+
+const divide = (x, y) => {
+  if (x < y) return 0;
+  return divide(x - y, y) + 1;
+}
+
+const modulo = (x, y) => {
+  if (!y) return undefined;
+  if (x < y) return x;
+  return modulo(x - y, y);
+}
