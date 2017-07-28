bc8e3cd28438c8fb5f70c09f4ba369fdca855e04
Lois Truby <loyslane@gmail.com>
2017-07-27 23:37:41 -0400

    completed queueStack challenge

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -2,3 +2,30 @@
  * Write a stack class. Once you're done,
  * implement a queue using two stacks.
  */
+
+class Stack {
+  constructor() {
+    this.arr = [];
+  }
+  add(input) {
+    return this.arr.push(input);
+  }
+  remove() {
+    return this.arr.pop();
+  }
+  get size() {
+    return this.arr.length;
+  }
+}
+
+const queueStack = (stack) => {
+  const tempStack = new Stack();
+  for (i = stack.size - 1; i >= 0; i--) {
+    tempStack.add(stack.remove());
+  }
+  tempStack.remove();
+  for (i = tempStack.size - 1; i >= 0; i--) {
+    stack.add(tempStack.remove());
+  }
+  return stack;
+}
