b7d305f88aeb4a2b6d9bfca0c1626cdd20c51c71
manishavi <manishalal972@gmail.com>
2017-07-28 10:42:11 -0500

    did the first part

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -2,3 +2,22 @@
  * Write a stack class. Once you're done,
  * implement a queue using two stacks.
  */
+ class Stack {
+ 	constructor() {
+ 		this.stack = [];
+		 this.size = 0;
+ 	}
+	 add(x) {
+		 this.stack.push(x);
+		 this.size = this.size++;
+	 }
+	 remove() {
+		 if(this.size > 0) {
+			 this.size = this.size--;
+		 }
+		 return this.stack.pop();
+	 }
+	 size() {
+		 this.size = 0;
+	 }
+ }
