877b243838cff6453ffc805e33f45e8815e90296
sfmajors373 <sfmajors373@gmail.com>
2017-07-28 11:33:52 -0400

    QueueStack: Queue remove not working correctly

--- constructors/constructors.js
+++ constructors/constructors.js
@@ -22,3 +22,139 @@
  * This is how you would structure the game objects in an actual game
  * application in Unity or another similar framework.
  */
+
+
+class NPC {
+  constructor(options){
+    this.hitpoints: options.hitpoints,
+    this.currentHits: options.currentHits,
+    this.strength: options.strength,
+    this.mp: options.mp
+  }
+  destroy() {}
+}
+// ************************************
+class Plant extends NPC {
+  constructor(options) {
+    super();
+  }
+}
+
+class FleshEatingDaisy extends Plant {
+  constructor(options) {
+    super();
+  }
+}
+// ***********************************
+class Animal extends NPC {
+  constructor() {
+    super();
+  }
+  findClosesbyRange(currentPosition){
+    if(target) {
+    }
+  }
+  speak(){
+    return 'Roaarrrrr';
+  }
+}
+
+class Bear extends Animal {
+  constructor() {
+    super();
+    hitpoints: 5000,
+  }
+}
+
+class Wolf extends Animal {
+  constructor() {
+    super();
+    hitpoints: 2000,
+  }
+}
+// ***********************************
+class Humanoid extends NPC {
+  constructor(options) {
+    super(options);
+    this.height: options.height,
+    this.gender: options.gender,
+    gear: {
+      head: null,
+      back: linenTunic,
+      legs: plainPants.
+      feet: boots,
+      accessories: null,
+    },
+    weapon: {
+      leftHand: null,
+      rightHand: null,
+    },
+    attackType: null,
+    resists: null,
+    this.occupation: options.occupation,
+  }
+  speak(){
+    return 'Ahhhh';
+  }
+  walk(){}
+}
+class Elf extends Humanoid {
+  constructor() {
+    super();
+    hitpoints: 700,
+    attackType: heal,
+    resists: magic,
+  }
+}
+
+class Orc extends Humanoid {
+  constructor() {
+    super();
+    hitpoints: 1200,
+    attackType: smash,
+    resists: physicalAttack,
+  }
+}
+
+class Human extends Humanoid {
+  constructor(options) {
+    super(options);
+    this.clan = options.clan,
+    this.language = options.language,
+    this.name = options.name
+  }
+}
+// ********************************
+class Soldier extends Human {
+  constructor() {
+    super();
+    hitpoints: 1000,
+    attackType: weapon,
+  }
+  shootGun() {}
+}
+
+class Bandit extends Human {
+  constructor() {
+    super();
+    hitpoints: 500,
+    attackType: steal
+  }
+}
+
+class Peasant extends Human {
+  constructor() {
+    super();
+    hitpoints: 500,
+    attackType: magic,
+    resists: physicalAttack && magic,
+  }
+}
+
+
+const bob = new Human({
+  hp: 50,
+  strength: 10,
+  speed: 5,
+  height: 80,
+  etc, etc...
