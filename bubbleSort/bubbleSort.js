2d92088670cc99561fa665839af1d491a81e4945
mixelpixel <mixelpix@mac.com>
2017-07-28 12:22:41 -0400

    ofc this dsnt wrk

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -1,22 +1,35 @@
 const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    let modify = 0;
-    let current = arr[i];
-    let next = arr[i + 1];
-    while (next !== undefined) {
-      console.log(`BEFORE: current index value: ${current}; next index: ${next}`);
-      if (next < current) {
-          arr[i] = next;
-          arr[i = 1] = current;
-          console.log(`AFTER: current index value: ${current}; next index: ${next}`);
-          modify++;
-      }
-    if (modify !== 0) {
-      // bubbleSort(arr);
-      console.log('more to sort', arr)
-    }
-    }
-  } return arr;
+  // compare first array element to second
+  if (arr[0] > arr[1]) {
+    arr[0] = arr[1];
+    arr[1] = arr[0];
+  }
+  return arr;
 };
 
+
 console.log(bubbleSort([ 2, 1, 3 ]))
+
+
+
+
+// const bubbleSort = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     let modify = 0;
+//     let current = arr[i];
+//     let next = arr[i + 1];
+//     while (next !== undefined) {
+//       console.log(`BEFORE: current index value: ${current}; next index: ${next}`);
+//       if (next < current) {
+//           arr[i] = next;
+//           arr[i = 1] = current;
+//           console.log(`AFTER: current index value: ${current}; next index: ${next}`);
+//           modify++;
+//       }
+//     if (modify !== 0) {
+//       // bubbleSort(arr);
+//       console.log('more to sort', arr)
+//     }
+//     }
+//   } return arr;
+// };
