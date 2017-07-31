15b98bc212a627803bc74284d2fba3e2ad3f60aa
mixelpixel <mixelpix@mac.com>
2017-07-31 12:32:38 -0400

    thinking

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -1,15 +1,23 @@
 // Code Challenge # 16
-
 /**
  * Given a single input string, write a function that produces all possible anagrams
  * of a string and outputs them as an array. At first, don't worry about
  * repeated strings.  What time complexity is your solution?
  *
  * Extra credit: Deduplicate your return array.
- */
 
-/**
   * example usage:
   * const anagrams = allAnagrams('abc');
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+
+// INPUT:    a string
+// FUNCTION: return all possible combinations of letterss
+// OUTPUT:   array of string permutations
+
+// SOLUTION: slice off the first letter, swap the next two letters, recombine the swapped letters with the first letter... Hmmm...
+
+*/
+
+cont allAnagrams = (str) => {
+
+}
