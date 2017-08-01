a70ff2d938c35ccb88de2ac96fb8ea0fbc25fc4d
mixelpixel <mixelpix@mac.com>
2017-08-01 11:58:47 -0400

    refactoring

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -22,25 +22,33 @@
 */
 
 const allAnagrams = (str) => {
-  x = []
-  if (str.length === 0) {
-    x.push(str);
-    return x;
-  };
-  if (str.length === 1) {
-    x.push(str);
-    return `one character is already all anagram permutations: ${x}`;
-  };
-  if (str.length === 2 && str[0] != str [1]) {
-    str[0] = str[1];
-    str[1] = str[0];
-    x.push(str);
-    return `swapped: ${x}`;
-  } else {
-    x.push(str);
-    return `there's nothing to do with: ${x}`;
-  };
+  arr = str.split('');
+
+
+
+  return str, arr;
 }
+
+// const allAnagrams = (str) => {
+//   x = []
+//   if (str.length === 0) {
+//     x.push(str);
+//     return x;
+//   };
+//   if (str.length === 1) {
+//     x.push(str);
+//     return `one character is already all anagram permutations: ${x}`;
+//   };
+//   if (str.length === 2 && str[0] != str [1]) {
+//     str[0] = str[1];
+//     str[1] = str[0];
+//     x.push(str);
+//     return `I took your ${str} and swapped it: ${x}`; // <--- oh, ha... yeah...
+//   } else {
+//     x.push(str);
+//     return `there's nothing to do with: ${x}`;
+//   };
+// }
 
 // TEST SUITE
 // Empty string
@@ -51,3 +59,22 @@ console.log(allAnagrams('a'));
 // Two character string
 console.log(allAnagrams('aa'));
 console.log(allAnagrams('ab'));
+
+
+// FACTORIAL
+const nFactorial = (n) => {
+  // factorial example: !5 = 5 * 4 * 3 * 2 * 1
+  // return the factorial of `n`
+
+  // // version 1 √
+  if (n === 1) return n;
+  return n * nFactorial(n - 1);
+
+  // // version 2: ternary solution √
+  // // declare variableName = ifCondition ? thenThis : otherwiseThat;
+  // const factorial = (n === 1) ? 1 : n * nFactorial(n - 1);
+  // return factorial;
+
+  // // version 2.1 √
+  // return (n === 1) ? n : n * nFactorial(n - 1);
+};
