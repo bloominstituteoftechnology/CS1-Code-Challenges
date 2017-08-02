b188984cc5a81d28bbf99e175a79eee144288cd0
mixelpixel <mixelpix@mac.com>
2017-08-02 10:36:08 -0400

    cc17 insertionSort wip

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -1,4 +1,5 @@
 /**
+  CODE CHALLENGE #17
  * Insertion sort is a basic sorting algorithm. Insertion sort
  * iterates over an array, growing a sorted array behind the current location.
  * It takes each element from the input and finds the spot, up to the current point,
