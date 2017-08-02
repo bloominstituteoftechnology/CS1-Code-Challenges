ef7b8d10d66d7332e1ac5c08c5c0fd0ce3dae0b0
mixelpixel <mixelpix@mac.com>
2017-08-02 12:21:06 -0400

    cc18 rotateImage wip

--- rotateImage/rotateImage.js
+++ rotateImage/rotateImage.js
@@ -1,2 +1,36 @@
+// cc18 rotateImage
+
 // Given an image represented by an NxN matrix, where each pixel in the image is an integer from 0 - 9,
 // write a method to rotate the image by 90 degrees. Can you do this in place?
+
+/*
+INPUT: (9x9 image matrix)
+const exampleImageArray = [
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+  [ 0. 1. 2. 3. 4. 5. 6. 7. 8. 9 ],
+]
+
+FUNCTION: 90 degree clockwise rotation
+
+OUTPUT:
+[
+  [ 0. 0. 0. 0. 0. 0. 0. 0. 0 ],
+  [ 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+  [ 2, 2, 2, 2, 2, 2, 2, 2, 2 ],
+  [ 3, 3, 3, 3, 3, 3, 3, 3, 3 ],
+  [ 4, 4, 4, 4, 4, 4, 4, 4, 4 ],
+  [ 5, 5, 5, 5, 5, 5, 5, 5, 5 ],
+  [ 6, 6, 6, 6, 6, 6, 6, 6, 6 ],
+  [ 7, 7, 7, 7, 7, 7, 7, 7, 7 ],
+  [ 8, 8, 8, 8, 8, 8, 8, 8, 8 ],
+  [ 9, 9, 9, 9, 9, 9, 9, 9, 9 ],
+]
+
+*/
