5f4231ac4a50ae2067f8fbaba07704545c1e80e5
mixelpixel <mixelpix@mac.com>
2017-08-03 12:10:46 -0400

    truth table

--- logicGates/logicGates.js
+++ logicGates/logicGates.js
@@ -9,24 +9,48 @@
  * All functions should return a 1 for true and a 0 for false.
  */
 
+// (Not And) operator
+// T !&& T
+// T !&& F
+// F !&& T
+// F !&& F
 const NAND = (x, y) => {
 	// You can use whatever JS operators that you would like: &&, ||, !
 };
 
+
+// Not operator
+// !T --> F
+// !F --> T
 const NOT = (n) => {
 	// Do not use !, &&, or ||
 };
 
+// AND
+// T && T ---> T
+// T && F ---> F
+// F && T ---> F
+// F && F ---> F
 const AND = (x, y) => {
 	// Do not use &&, ||, or !
 	// You can use any of the functions that you have already written
 };
 
+// INclusive OR
+// T || T ---> T
+// T || F ---> T
+// F || T ---> T
+// F || F ---> F
 const OR = (x, y) => {
 	// Do not use ||, &&, or !
 	// You can use any of the functions that you have already written
 };
 
+// EXclusive OR
+// T X T ---> F
+// T X F ---> T
+// F X T ---> T
+// F X F ---> F
 const XOR = (x, y) => {
 	// Do not use ||, &&, or !
 	// You can use any of the functions that you have already written
