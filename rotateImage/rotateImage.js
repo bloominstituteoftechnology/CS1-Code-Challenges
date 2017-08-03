ad48741bf70df5b497a85809dee3bbbabdb595ea
mixelpixel <mixelpix@mac.com>
2017-08-03 03:26:12 -0400

    off by 1...

--- rotateImage/rotateImage.js
+++ rotateImage/rotateImage.js
@@ -3,6 +3,34 @@
 // Given an image represented by an NxN matrix, where each pixel in the image is an integer from 0 - 9,
 // write a method to rotate the image by 90 degrees. Can you do this in place?
 
+// for each row (array), take the column (index) and put each
+// corresponding index value in an array, so row 1-9 col (index) 0 become an array,
+// then row1-9 col (index) 1 and so on.
+
+const rotateImage = (arr) => {
+  const len = arr.length;
+  rotation = Array(len).fill().map(()=> []);
+  for (let col = 0; col < len; col++) {
+    for (let row = 0; row < len; row ++) {
+      rotation[col].push(arr[row][col]);
+    }
+  }
+  return rotation;
+}
+
+console.log(rotateImage([
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+]
+));
+
 /*
 INPUT: (9x9 image matrix)
 const exampleImageArray = [
