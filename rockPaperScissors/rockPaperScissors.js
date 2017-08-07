ce0ca056187f4cdea732db441d43652943921d02
mixelpixel <mixelpix@mac.com>
2017-08-07 12:16:00 -0400

    cc20 - rPS: rough

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,3 +1,5 @@
+// cc21 rockPaperScissor
+
 /*
  * * Write a function that generates every sequence of throws a single
  * * player could throw over a three-round game of rock-paper-scissors.
@@ -12,4 +14,42 @@
 
 const rockPaperScissors = () => {
 	// TODO: your solution here
+  const hand = {0: 'rock',
+                1: 'paper',
+                2: 'scissor'}
+  const hand = ['rock', 'paper', 'scissor']
+
+};
+
+
+
+const allAnagrams = (str) => {
+
+  if (str.length < 2) return str;
+
+  const result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let firstLetter = str[i];
+    let restOfString = str.slice(0, i) + str.slice(i + 1, str.length);
+
+    for (let sub of allAnagrams(restOfString)) {
+      result.push(firstLetter + sub);
+    }
+  }
+  return result;
+}
+
+
+const allAnagrams = (str, start = '') => {
+  if (str.length === 1) return [start + str];
+  const anagrams = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const result = allAnagrams(str.substr(0, i) + str.substr(i + 1), str[i]);
+    for (let j = 0; j < result.length; j++) {
+      anagrams.push(start + result[j]);
+    }
+  }
+  return anagrams;
 };
