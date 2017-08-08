f6f1a3b8797e45ab7d8ea1f35b6567431c7f985a
bdurb <brian@rpmgreenville.com>
2017-08-08 12:03:46 -0400

    working

---
+++ allAnagrams/allAnagrams.js
@@ -0,0 +1,13 @@
+/**
+ * Given a single input string, write a function that produces all possible anagrams
+ * of a string and outputs them as an array. At first, don't worry about
+ * repeated strings.  What time complexity is your solution?
+ *
+ * Extra credit: Deduplicate your return array.
+ */
+
+/**
+  * example usage:
+  * const anagrams = allAnagrams('abc');
+  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+  */
--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -4,30 +4,41 @@
  */
 class Stack() {
   constructor() {
-    this.stack = [];
+    this.storage = [];
     this.size = 0;
   }
 
   add(item) {
-    this.stack.push(item);
+    this.storage.push(item);
     this.size = this.size + 1;
   }
   remove() {
     if (this.size > 0) {
       this.size = this.size -1;
     }
-    return this.stack.pop();
+    return this.storage.pop();
   }
   size() {
     this.size = 0;
   }
 };
 
-const queue = () => {
-  const stack1 = new Stack();
-  const stack2 = new Stack(); 
-}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+  enqueue(item) {
+    this.stack1.add(item);
+  }
 
-enqueue(item) {
-  stack1.add(item)
+  dequeue() {
+    if (this.stack2.length === 0) {
+      const length = this.stack1.length;
+      for (let i = 0; i < length; i++) {
+        this.stack2.add(this.stack1.remove());
+      }
+    }
+    return this.stack2.remove();
+  }
 }
