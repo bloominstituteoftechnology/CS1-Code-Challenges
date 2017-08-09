27969ed406186eee910c9ce60b2762dd4f80870f
mixelpixel <mixelpix@mac.com>
2017-08-09 11:58:56 -0400

    cc22 quickSort: close...

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -5,5 +5,27 @@
  * https://www.khanacademy.org/computing/computer-science/algorithms#quick-sort
  */
 const quickSort = (nums) => {
+  if (nums.length < 2) {
+    return nums;
+  }
+  // console.log(nums)
 
+  const pivot = nums[0]
+  let lessThan = [];
+  let greaterThan = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < pivot) {
+      lessThan.push(nums[i]);
+      // console.log('less than array: ', lessThan)
+    } else {
+      greaterThan.push(nums[i]);
+      // console.log('GRTR than array: ', greaterThan)
+    }
+  };
+  return quickSort(lessThan).concat(pivot, quickSort(greaterThan));
 };
+
+// TEST SUITE
+// console.log(quickSort([ 9, 8, 7, 555, 632, 345, 4, 3, 2, 1]));
+console.log(quickSort([ 9, 8, 7, 555, 632 ]));
