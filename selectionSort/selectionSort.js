bdf9b92909764a6d4dd585feb9eeef89046ebc2e
mixelpixel <mixelpix@mac.com>
2017-08-09 12:50:02 -0400

    cc23 selectionSort √

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -7,5 +7,20 @@
  */
 
 const selectionSort = (arr) => {
-
+  const sorted = Array.from(arr);
+  let minIndex, i, j;
+  for (i = 0; i < sorted.length; i++) {
+    minIndex = i;
+    for (j = i; j < sorted.length; j++) {
+      if (sorted[j] < sorted[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [sorted[i], sorted[minIndex]] = [sorted[minIndex], sorted[i]];
+  }
+  return sorted;
 };
+
+
+// TEST SUITE
+console.log(selectionSort([9, 7, 8, 5, 6, 4, 5, 2, 3, 1]));
