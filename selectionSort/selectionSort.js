401d822c5b829226fe5fcefb5f2593d0064a2d5f
wlharvey4 <pinecone062@gmail.com>
2017-08-10 09:41:33 -0700

    finished selectionSort

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -8,4 +8,37 @@
 
 const selectionSort = (arr) => {
 
+  const exch = (i, j) => {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    exch(i, min);
+  }
+
+  return arr;
 };
+
+const randomize = (arr) => {
+  for (let i = arr.length; i--; i > 1) {
+    const j = Math.floor(Math.random() * i);
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
+
+const r_arr = randomize([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(r_arr);
+const s_arr = selectionSort(r_arr);
+console.log(s_arr);
