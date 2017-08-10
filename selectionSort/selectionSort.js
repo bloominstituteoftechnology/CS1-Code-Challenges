2f72fdbc64331e5f168c491453f591fb44e9e9aa
Latoyya Smith <latoyyas@gmail.com>
2017-08-10 10:39:36 -0500

    completed selectionSort.js

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -7,5 +7,27 @@
  */
 
 const selectionSort = (arr) => {
-
+  let smallest , temp;
+  const length = arr.length;
+  if(length === 0 || length === 1) {
+    return arr;
+  }
+  for(let i = 0; i < length; i++) {
+    smallest = i;
+    for(let j = i + 1; j < length; j++) {
+        if(arr[j] < arr[smallest]) {
+            smallest = j;
+        }
+    }
+    temp = arr[i];
+    arr[i] = arr[smallest];
+    arr[smallest] = temp;
+  }
+  return arr;
 };
+
+console.log('\nTime Complexity O(n^2)');
+console.log(selectionSort([16, 98, 87, 87, 55, 99, 36, 100, 187])); // [16, 36, 55, 87, 87, 98, 99, 100, 107]
+console.log(selectionSort([44, 87, 32, 21, 3])); // [3, 21, 32, 44, 87]
+console.log(selectionSort([25])); // [25]
+console.log(selectionSort([])); // []
