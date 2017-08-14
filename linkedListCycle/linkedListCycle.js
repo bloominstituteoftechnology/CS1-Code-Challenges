f79f81704dce048789458ea725a8994440ded423
Latoyya Smith <latoyyas@gmail.com>
2017-08-14 12:20:19 -0500

    completed linkedListCycle.js

--- linkedListCycle/linkedListCycle.js
+++ linkedListCycle/linkedListCycle.js
@@ -29,3 +29,33 @@
  * Constraint 3: Do not mutate the original nodes in any way
  * Hint: Search for Floyd's Tortoise and Hare algorithm.
  */
+
+ class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const hasCycle = (head) => {
+  let fastPtr = head;
+  let slowPtr = head;
+  
+  while (fastPtr !== null && fastPtr.next !== null) {
+    fastPtr = fastPtr.next.next;
+    slowPtr = slowPtr.next;
+    if (slowPtr === fastPtr) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const nodeA = new Node('A');
+const nodeB = nodeA.next = new Node('B');
+const nodeC = nodeB.next = new Node('C');
+const nodeD = nodeC.next = new Node('D');
+const nodeE = nodeD.next = new Node('E');
+console.log(hasCycle(nodeA)); // => false
+nodeE.next = nodeB;
+console.log(hasCycle(nodeA)); // => true
