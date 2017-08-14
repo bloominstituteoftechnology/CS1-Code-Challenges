8ec89c64da77a95d19ac2b378290dd9e2bac7413
mixelpixel <mixelpix@mac.com>
2017-08-14 15:46:58 -0400

    idem

--- blueSquares/script.js
+++ blueSquares/script.js
@@ -1,5 +1,18 @@
-$(".test").on("click", function() {
+// $("#test").toggle(function() {
+//     $(this).css("background", "red");
+// },
+// function() {
+//     $(this).css("background", "");
+// });
 
-  $(this).css{"background", "blue"};
 
+$(".flex-item-1").on("mousedown", function() {
+    $(this).toggleClass('back-red');
+})
+.on("mouseup", function(e) {
+    $(this).toggleClass('back-red');
 });
+
+
+
+// https://www.w3schools.com/jquery/jquery_get_started.asp
