cbb895a66ee55be69b8af4e3920c1710196239d0
mixelpixel <mixelpix@mac.com>
2017-08-15 12:19:51 -0400

    cc26 blueSquares √

--- blueSquares/script.js
+++ blueSquares/script.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+    $('.flex-item').click(function() {
+        $(this).toggleClass('clicked');
+    });
+});
+
+
 // $("#test").toggle(function() {
 //     $(this).css("background", "red");
 // },
@@ -6,13 +13,24 @@
 // });
 
 
-$(".flex-item-1").on("mousedown", function() {
-    $(this).toggleClass('back-red');
-})
-.on("mouseup", function(e) {
-    $(this).toggleClass('back-red');
-});
-
+// $("#flex-item-1").on("mousedown", function() {
+//     $(this).toggleClass('back-red');
+// })
+// .on("mouseup", function(e) {
+//     $(this).toggleClass('back-red');
+// });
+//
+//
+// $(document).ready(function(){
+//   $('.flex-item').click(function(){
+//     $(this).toggleClass('blue-box');
+//   });
+//   $('.box').mouseenter(function(){
+//     $(this).addClass('animated pulse');
+//   }).mouseleave(function(){
+//     $(this).removeClass('animated pulse');
+//    });
+//  });
 
 
 // https://www.w3schools.com/jquery/jquery_get_started.asp
