c82500031ca2e3161e1dfd225b5368d9cb04d900
mixelpixel <mixelpix@mac.com>
2017-08-16 12:20:12 -0400

    cc28 stackOfPlates

--- stackOfPlates/stackOfPlates.js
+++ stackOfPlates/stackOfPlates.js
@@ -1,4 +1,5 @@
-/* Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+/* cc28 stackOfPlates
+ * Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
  * Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold.
  * Implement a data structure SetOfStacks that mimics this.
  * SetOfStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity.
