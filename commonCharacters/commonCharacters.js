5858bc30f7b7a41675cd57b2d55172029b44a353
mixelpixel <mixelpix@mac.com>
2017-08-18 12:03:15 -0400

    cc30 commonCharacters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -1,9 +1,8 @@
-/**
- * Common Characters: 
- * Write a function that accepts two strings as arguments, and returns only the characters that are common to both strings. * 
- * Your function should return the common characters in the same order that they appear in the first argument. 
- * Do not return duplicate characters and ignore whitespace in your returned string.  * 
- * Example: commonCharacters('acexivou', 'aegihobu')  * 
+/** cc30 commonCharacters
+ * Common Characters:
+ * Write a function that accepts two strings as arguments, and returns only the characters that are common to both strings. *
+ * Your function should return the common characters in the same order that they appear in the first argument.
+ * Do not return duplicate characters and ignore whitespace in your returned string.  *
+ * Example: commonCharacters('acexivou', 'aegihobu')  *
  * Returns: 'aeiou'
 */
-
