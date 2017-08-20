5f1e25e37735e99d5f85c4b35424cf3ecc9eebed
wlharvey4 <pinecone062@gmail.com>
2017-08-20 09:40:00 -0700

    commonCharacters working

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -7,3 +7,26 @@
  * Returns: 'aeiou'
 */
 
+const commonCharacters = (str1, str2) => {
+  let str = '';
+
+  const str1Arr = str1.split('');
+  let char;
+  while( str1Arr.length ) {
+    char = str1Arr.shift();
+    if (!(char === '\040' || str.includes(char))) { // ignore spaces and duplicates
+      if(str2.includes(char)) {
+        str += char;
+      }
+    }
+  }
+
+  return str;
+}
+
+/***********************************************************************/
+
+const str1 = 'acex ivou';
+const str2 = 'aegi hobu';
+const solution = commonCharacters(str1, str2);
+console.log(`str1: ${str1}\nstr2: ${str2}\nsolution: ${solution}`);
