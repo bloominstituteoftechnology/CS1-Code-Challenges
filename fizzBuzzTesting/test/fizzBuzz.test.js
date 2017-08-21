73e450f163c4a4c567f313520cd3e7d0bb6350d1
mixelpixel <mixelpix@mac.com>
2017-08-21 17:46:52 -0400

    cc31 fizzBuzzTesting √

--- fizzBuzzTesting/test/fizzBuzz.test.js
+++ fizzBuzzTesting/test/fizzBuzz.test.js
@@ -4,35 +4,41 @@ const assert = require('chai').assert;
 
 const chai = require('chai');
 const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
+// const sinonChai = require('sinon-chai');
 
 const expect = chai.expect;
-chai.use(sinonChai);
+// chai.use(sinonChai);
 
 
 describe('FizzBuzz Test', () => {
-
-  const fizzBuzz = fizzBuzzScript.fizzBuzz
+  const fizzBuzz = fizzBuzzScript.fizzBuzz;
   describe('#1: fizzBuzz imports from the script file and ', () => {
     it('should be a function', () => {
       expect(fizzBuzz).to.be.a('function');
     });
   });
 
-  describe('#2: fizzBuzz return value types', () => {
+  describe('#2: fizzBuzz returns correct value types,', () => {
     it('should return a `String` when conditions are met', () => {
       assert.typeOf(fizzBuzz(15), 'string');
+      expect(fizzBuzz(15)).to.be.a('string');
       assert.typeOf(fizzBuzz(9), 'string');
+      expect(fizzBuzz(9)).to.be.a('string');
       assert.typeOf(fizzBuzz(10), 'string');
+      expect(fizzBuzz(10)).to.be.a('string');
     });
     it('should return a `Number` when conditions are not met', () => {
       assert.typeOf(fizzBuzz(4), 'number');
+      expect(fizzBuzz(4)).to.be.a('number');
     });
-    console.log(fizzBuzz(44));
+    // console.log(fizzBuzz(44));
     it('should return the given argument when conditions are not met', () => {
       assert.equal(fizzBuzz(44), 44);
-      // expect(fizzBuzz(44).to.equal(44));
-      // expect(fizzBuzz(44).to.be(44));
+      expect(fizzBuzz(44)).equals(44);
+      assert.equal(fizzBuzz('x'), 'x');
+      expect(fizzBuzz('x')).equals('x');
+      assert.equal(fizzBuzz(true), true);
+      expect(fizzBuzz(true)).equals(true);
     });
   });
 
@@ -40,8 +46,7 @@ describe('FizzBuzz Test', () => {
     // console.log(fizzBuzz(9));
     it('should return `fizz` when the argument % 3 === 0', () => {
       assert.equal(fizzBuzz(6), 'fizz');
-      // expect(fizzBuzz(9).to.equal('fizz'));
-      // expect(fizzBuzz(9).to.be('fizz'));
+      expect(fizzBuzz(9)).to.equal('fizz');
     });
   });
 
