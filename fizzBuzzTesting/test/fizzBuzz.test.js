4ee5d7795370642a59e98f07fd7dbe724a855212
mixelpixel <mixelpix@mac.com>
2017-08-21 12:56:54 -0400

    cc31 fizzBuzzTesting √

--- fizzBuzzTesting/test/fizzBuzz.test.js
+++ fizzBuzzTesting/test/fizzBuzz.test.js
@@ -29,33 +29,37 @@ describe('FizzBuzz Test', () => {
       assert.typeOf(fizzBuzz(4), 'number');
     });
     console.log(fizzBuzz(44));
-    it.skip('should return the given argument when conditions are not met', () => {
-      expect(fizzBuzz(44).to.equal(44));
+    it('should return the given argument when conditions are not met', () => {
+      assert.equal(fizzBuzz(44), 44);
+      // expect(fizzBuzz(44).to.equal(44));
       // expect(fizzBuzz(44).to.be(44));
     });
   });
 
   describe('fizz', () => {
     // console.log(fizzBuzz(9));
-    it.skip('should return `fizz` when the argument % 3 === 0', () => {
+    it('should return `fizz` when the argument % 3 === 0', () => {
+      assert.equal(fizzBuzz(6), 'fizz');
       // expect(fizzBuzz(9).to.equal('fizz'));
-      expect(fizzBuzz(9).to.be('fizz'));
+      // expect(fizzBuzz(9).to.be('fizz'));
     });
   });
 
   describe('buzz', () => {
     // console.log(fizzBuzz(10));
-    it.skip('should return `buzz` when the argument % 3 === 0', () => {
+    it('should return `buzz` when the argument % 3 === 0', () => {
+      assert.equal(fizzBuzz(10), 'buzz');
       // expect(fizzBuzz(10).to.equal('buzz'));
-      expect(fizzBuzz(10).to.be('buzz'));
+      // expect(fizzBuzz(10).to.be('buzz'));
     });
   });
 
   describe('fizz and buzz', () => {
     // console.log(fizzBuzz(15));
-    it.skip('should return `fizzbuzz` when the argument % 3 === 0', () => {
+    it('should return `fizzbuzz` when the argument % 3 === 0', () => {
+      assert.equal(fizzBuzz(15), 'fizzbuzz');
       // expect(fizzBuzz(15).to.equal('fizzbuzz'));
-      expect(fizzBuzz(15).to.be('fizzbuzz'));
+      // expect(fizzBuzz(15).to.be('fizzbuzz'));
     });
   });
 });
