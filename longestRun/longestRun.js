1a3026af6f65a3c347feac11d1b10cadb0236099
mixelpixel <mixelpix@mac.com>
2017-08-22 12:29:03 -0400

    cc32 longestRun

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -1,8 +1,11 @@
-/**
+/** cc32 longestRun
 * Longest Run
-* Write a function that, given a string, finds the longest run of identical characters and returns an array containing the start and end indices of that run. 
-* If there are two runs of equal length, return the first one. Return [0,0] for no runs.
-* Examples: 
+* Write a function that, given a string, finds the longest run of
+* identical characters and returns an array containing the start
+* and end indices of that run.
+* If there are two runs of equal length, return the first one.
+* Return [0,0] for no runs.
+* Examples:
 * Input: "abbbcc"	Output: [ 1, 3 ]
 * Input: "mississippi" Output: [ 2, 3 ]
 * Input: "abcdefgh" Output: [ 0, 0 ]
