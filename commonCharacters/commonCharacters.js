be8c7b7e39f553e3445a41429e7ca45707c3d5a1
bdurb <brian@rpmgreenville.com>
2017-08-22 12:05:40 -0400

    commonCharacters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -7,14 +7,29 @@
  * Returns: 'aeiou'
 */
 
-const commonChars = (str1, str2) => {
-  newStr = '';
+// const commonChars = (str1, str2) => {
+//   newStr = '';
+//   for (let i = 0; i < str1.length; i++) {
+//     for (let j = 0; j < str2.length; j++) {
+//       if (str1[i] === str2[j]) {
+//         newStr.concat(str1[i]);
+//       }
+//     }
+//   }
+//   return newStr;
+// }
+
+const commonCharacters = (str1, str1) => {
+  const obj = {' ': 3}
+  let str = '';
+  for (let i = 0; i < str2.length; i++) {
+    if (!obj[str2[i]]) obj[str2[i]] = 1;
+  }
   for (let i = 0; i < str1.length; i++) {
-    for (let j = 0; j < str2.length; j++) {
-      if (str1[i] === str2[j]) {
-        newStr.concat(str1[i]);
-      }
+    if (obj[str1[i]] === 1) {
+      obj[str1[i]] = 2;
+      str += str1[i];
     }
   }
-  return newStr;
-}
+  return str;
+};
