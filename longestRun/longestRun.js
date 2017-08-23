a650ca926152a542538136f0e9723dbf42a0ed5b
wlharvey4 <pinecone062@gmail.com>
2017-08-23 12:29:34 -0700

    longestRun completed

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -8,3 +8,34 @@
 * Input: "abcdefgh" Output: [ 0, 0 ]
 * Input: "aabbbcccc" Output: [ 5, 4 ]
 */
+
+const longestRun = (str) => {
+
+  let index = 0, start = 0, current = str[0], a = 0, b = 0;
+
+  const update = () => {
+    if (index - start - 1 > b - a) {
+      a = start, b = index - 1;
+    }
+  }
+
+  for (let s of str) {
+    if (s !== current) {
+      update();
+      start = index, current = s;
+    }
+    index++;
+  }
+  update();
+
+  return [a, b];
+}
+
+const str1 = 'abbbcc';
+console.log(longestRun(str1));
+const str2 = 'mississippi';
+console.log(longestRun(str2));
+const str3 = 'abcdefgh';
+console.log(longestRun(str3));
+const str4 = 'aabbbcccc';
+console.log(longestRun(str4));
