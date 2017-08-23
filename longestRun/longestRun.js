073ac2db5328828010a9bdf61d2f9f0c9fde32fa
Lois Truby <loyslane@gmail.com>
2017-08-23 12:25:42 -0400

    completed longestRun challenge

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -8,3 +8,30 @@
 * Input: "abcdefgh" Output: [ 0, 0 ]
 * Input: "aabbbcccc" Output: [ 5, 4 ]
 */
+
+const longestRun = (str) => {
+  const ansArr = [0, 0];
+  const strArr = Array.from(str);
+  let longestRunCount = 0;
+  let letterCount = 0;
+  let tempIndex;
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === strArr[i + 1] && strArr[i] !== strArr[i - 1]) {
+      tempIndex = i;
+      letterCount = 1;
+    } else if (strArr[i] === strArr[i + 1]) {
+      letterCount++;
+    } else if (letterCount === longestRunCount && strArr[i + 1] === undefined && strArr[i] === strArr[i - 1]) {
+        ansArr[0] = tempIndex;
+        ansArr[1] = i;
+    } else {
+      if (letterCount > longestRunCount) {
+        longestRunCount = letterCount;
+        ansArr[0] = tempIndex;
+        ansArr[1] = i;
+        letterCount = 0;
+      }
+    }
+  };
+  return ansArr;
+};
