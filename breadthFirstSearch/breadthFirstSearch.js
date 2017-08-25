370d169eafb90117462545fee78790be04d9f2b0
Ely Alamillo <el0013@gmail.com>
2017-08-24 19:16:45 -0500

    challenge completed, go over solution

--- breadthFirstSearch/breadthFirstSearch.js
+++ breadthFirstSearch/breadthFirstSearch.js
@@ -15,3 +15,15 @@
  * };
  * breadthFirstSearch(tree, 2);// will return true before it recursively searches `z`
  */
+
+ const breadthFirstSearch = (tree, searchTerm) => {
+  let queue = Object.values(tree);
+  while (queue.length > 0) {
+    value = queue.shift();
+    if (value === searchTerm) return true;
+    if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+      queue = queue.concat(Object.values(value));
+    }
+  }
+  return false;
+};
