9c902d4a0bc21216000b5755c4cf636f10d32d60
sfmajors373 <sfmajors373@gmail.com>
2017-08-29 12:53:14 -0400

    Adjustment to daily challenge

--- armstrongNumbers/armstrongNumbers.js
+++ armstrongNumbers/armstrongNumbers.js
@@ -3,11 +3,10 @@
 // For example with the number 153 there are 3 digits so you would add together the cubed values of all the digits like this: 1^3 + 5^3 + 3^3 === 153
 
 function isArmstrongNumber(n){
-  const length = n.toString().length;
   const arr = n.toString().split('');
   let acc = 0;
   for (let i = 0; i < arr.length; i++) {
-    acc = Math.pow(Number(arr[i]), length) + acc;
+    acc = Math.pow(Number(arr[i]), arr.length) + acc;
   }
   if (acc === n) {
     return true;
