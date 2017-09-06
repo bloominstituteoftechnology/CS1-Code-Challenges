bdebd15bce5797d9632606e704ba551b79109d38
mixelpixel <mixelpix@mac.com>
2017-09-06 14:13:57 -0400

    cc42 collatzSequence √

--- cc42 collatzSequence/solution.js
+++ cc42 collatzSequence/solution.js
@@ -1,4 +1,5 @@
 // https://repl.it/student/assignments/351164/model_solution?fromSubmissionId=1498763
+// https://youtu.be/aW-KYfWrsMk
 
 function collatzSequence(n) {
   let m = n;
@@ -14,3 +15,24 @@ function collatzSequence(n) {
   }
   return arr;
 }
+
+/*
+function collatzSequence(n) {
+  // create variable to hold n called o
+  // define recursive function that takes in (m, arr)
+    // base case of recursive solution => n === 1
+    // if m % 2 === 0 m = m / 2
+    // else m = m * 3 + 1
+    // push to arr
+    // return recursive function (m, arr);
+  // return recursive function(o, [o]);
+  function recurse(m, arr) {
+    if (m === 1) return arr;
+    if (m % 2 === 0) m = m / 2;
+    else m = m * 3 + 1;
+    arr.push(m);
+    return recurse(m, arr);
+  }
+  return recurse(n, [n]);
+}
+*/
