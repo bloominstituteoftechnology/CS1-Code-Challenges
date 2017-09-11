e1575b1138a1607b79d089383dc85bacaefba048
mixelpixel <mixelpix@mac.com>
2017-09-11 12:09:35 -0400

    cc45 deepEquality

--- cc45 deepEquality/deepEquality.js
+++ cc45 deepEquality/deepEquality.js
@@ -1,3 +1,33 @@
-/* cc45 deepEquality
+/* cc45 deepEquality https://repl.it/student/submissions/1538402
+Write a function that, given two objects, returns whether or not the two
+are deeply equivalent--meaning the contents of the two objects are equal for all keys and sub-keys.
 
+Examples:
+
+const johnA = {
+  name: 'John',
+  address: {
+    line1: '321 Anytown',
+    line2: 'Stoke-on-Trent'
+  }
+};
+
+const johnB = {
+  name: 'John',
+  address: {
+    line1: '321 Anytown',
+    line2: 'Stoke-on-Trent'
+  }
+};
+
+const johnC = {
+  name: 'John Charles',
+  address: {
+    line1: '321 Anytown',
+    line2: 'Stoke-on-Trent'
+  }
+};
+
+deepEquals(johnA, johnB); // true
+deepEquals(johnA, johnC); // false;
 */
