5046326c9faf69e127ebc6849f874360a220b1ff
mixelpixel <mixelpix@mac.com>
2017-09-11 13:23:33 -0400

    cc44 sumAndProduct

--- cc44 sumAndProduct/solution.js
+++ cc44 sumAndProduct/solution.js
@@ -1,5 +1,5 @@
 // https://repl.it/student/assignments/367850/model_solution?fromSubmissionId=1521337
-//
+// https://youtu.be/fnf9Vm7BP-Y
 
 function sumAndProduct(sum, product) {
   for (let i = 0; i <= sum / 2; i++) {
