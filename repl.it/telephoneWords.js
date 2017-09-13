1d64b157a3a32df724df82babe33d992fbc5c9c4
wlharvey4 <pinecone062@gmail.com>
2017-09-12 21:44:00 -0700

    telephoneWords working

--- repl.it/telephoneWords.js
+++ repl.it/telephoneWords.js
@@ -32,83 +32,28 @@ const phoneDigitsToLetters = {
   9: 'WXYZ'
 };
 
-// => {'0': '0', '1': '1', 'ABC': '2', 'DEF': '3', ... 'WXYZ': '9'}
-const lettersToPhoneDigits = (() => {
-  const entries = Object.entries(phoneDigitsToLetters);
-  const obj = {};
-  entries.forEach(e => {
-    obj[e[1]] = e[0];
-  });
-  return obj;
-})();
-
 function telephoneWords(str) {
+  const solution = [];
 
-  // helper function 'digitArr'
-  // Turns a string of numbers into an array of numbers
-  // Input: String of Numbers
-  // Output: Array of Numbers
-  // '2345' => [2, 3, 4, 5]
-  const digitArr = str.split('');
+  // input:  array of 3-letter strings; and
+  // input:  word being build
+  // output: none; complete words added to solution array
+  const recursiveHelper = (arr, word) => {
+    // base case: add word to solution and return
+    if (arr.length === 0) return solution.push(word);
 
-  // helper function 'numtoLetters'
-  // Turns a number into an array of letters;
-  // Input: Number
-  // Output: Array of Characters (single Strings)
-  // 2 => ['A', 'B', 'C']
-  const numToLetters = (n) => {
-    return phoneDigitsToLetters[n].split('');
-  }
-
-  const lettersToNum = (l) => {
-    
-  }
-
-  // helper function 'permutator'
-  // Given an array of items, returns all permutations of those items;
-  // Input:  array of items to be permuted
-  // Output: array of permutations
-  // Example: 
-  const permutator = (inputArr) => {
-    let result = [];
-
-    const permute = (arr, m = []) => {
-      if (arr.length === 0) {
-        result.push(m)
-      } else {
-        for (let i = 0; i < arr.length; i++) {
-          let curr = arr.slice();
-          let next = curr.splice(i, 1);
-          permute(curr.slice(), m.concat(next))
-        }
-      }
+    // recurse with smaller array after adding letter to word
+    for (let c of arr[0]) {
+      recursiveHelper(arr.slice(1), word + c);
     }
+  };
 
-    permute(inputArr)
+  // create an array of 3-letter strings
+  const arrOfStr = str.split('').map(n => phoneDigitsToLetters[n]);
 
-    return result;
-  }
-
-  const combinor = (arrIn, arrOut) => {
-    return ['ADGJ', 'BDGJ'];
-  }
-
-  /* ======================================================================= */
-
-  // obtain an array of character arrays based upon the initial number
-  const allLetters = digitArr.map(n => numToLetters(n));
-  // '2345' => [['A','B',C'],['D','E','F',[...],[...]]
-
-  // permute each of the arrays of numbers
-  const allPermutedLetters = allLetters.map(set => permutator(set));
-  // [[['A','B','C'],['A','C','B'],...],[['D','E','F'],[...],...],[[][]],...]]]
-
-  //allPermutedLetters.forEach((g,i) => console.log(`[${i}]\n`, g));
-
-  const result = combinor(allPermutedLetters, []);
-  console.log(result);
-
-  return result;
+  // begin recursion with full array and empty word
+  recursiveHelper(arrOfStr, '');
+  return solution;
 }
 
-telephoneWords('2345');
+console.log(telephoneWords('2745'));
