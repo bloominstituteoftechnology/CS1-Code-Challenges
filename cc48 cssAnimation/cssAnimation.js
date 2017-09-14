7d2b3a1b7378c19b9428beba5fd52f60fa42849c
mixelpixel <mixelpix@mac.com>
2017-09-14 13:09:56 -0400

    cc48 cssAnimation √

---
+++ cc48 cssAnimation/cssAnimation.js
@@ -0,0 +1,11 @@
+/* cc48 cssAnimation https://repl.it/student/submissions/1571861
+Head over to this link: https://codepen.io/sperrye/full/oGvoWB/ and follow the instructions.
+
+Once you're finished, you'll submit the URL to your pen as your answer.
+https://codepen.io/mixelpix/pen/ZXYNxX?editors=1100
+
+- http://cssreference.io/animations/
+- https://webdesign.tutsplus.com/tutorials/a-beginners-introduction-to-css-animation--cms-21068
+- https://developer.mozilla.org/en-US/docs/Web/CSS/animation
+- https://cssanimation.rocks/portal/
+*/
