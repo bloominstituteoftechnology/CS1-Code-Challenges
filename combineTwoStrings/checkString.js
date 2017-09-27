d0c0dd773324121a7ed444a2d8b9efc97c68f85e
wlharvey4 <pinecone062@gmail.com>
2017-09-27 09:06:04 -0700

    multiple comments removed; explanation added

--- combineTwoStrings/checkString.js
+++ combineTwoStrings/checkString.js
@@ -1,21 +1,34 @@
 /* 
 checkString.js
 
-Given three strings, str1, str2, str3. str3 is said to be a shuffle of str1
-/* and str2 if it can be formed by interleaving the characters of str1 and
-/* str2 in such a manner that it maintains left to right ordering from each
-/* string. Given str1 = 'abc' and str2 = 'def', str3 = 'dabecf' is a valid
-/* shuffle since it preserves the character ordering of the two strings. 
+Given three strings, str1, str2, str3. str3 is said to be a shuffle of
+str1 and  str2 if it can  be formed by interleaving  the characters of
+str1  and str2  in  such a  manner  that it  maintains  left to  right
+ordering from each string. Given str1 = 'abc' and str2 = 'def', str3 =
+'dabecf' is a valid shuffle  since it preserves the character ordering
+of the two strings.
 
-So, given these three strings, write a function that detects whether str3 is
-a valid shuffle of str1 and str2.
+So, given these  three strings, write a function  that detects whether
+str3 is a valid shuffle of str1 and str2.
 
-*/
+   NOTE: This version uses a stack of states to correctly evaluate the
+   edge case  in which str1  and str2  have duplicate characters  at a
+   particular point.  When  duplicates are found, the  state is placed
+   on  the stack,  and  k is  placed  into a  cache.   If the  current
+   evaluation returns  false, but there  are more states  to evaluate,
+   then the  program will pop  the next state and  continue evaluating
+   from that point.  If duplicates are  found, but the current k value
+   is in  the cache,  then this duplicate  has already  been evaluated
+   once, and it is ignored.
+
+   The  recursive solution  handles evaluating  these duplicates  in a
+   much  more elegant  and  simple fashion  because recursion  handles
+   stacks of state automatically and behind-the-scenes.
+ */
 
 function checkString(str1, str2, str3) {
   let i = 0, j = 0, k = 0;
 
-  // Must keep state for case of duplicate characters
   const initial_state = {
     str1: str1,
     i: i,
@@ -24,29 +37,19 @@ function checkString(str1, str2, str3) {
     str3: str3,
     k: k
   };
-
-  // Use a cache of prior states to prevent infinite loop;
-  // just holds the k value of states that have duplicate characters
   const state_cache = [];
-
-  // Use a stack to hold states that need to be evaluated
   const state_arr = [initial_state];
 
-  // Continue looking so long as there is state on the stack
   while (state_arr.length > 0) {
-    // pop the next state to evaluate
     const state = state_arr.pop();
     let {str1, i, str2, j, str3, k} = state;
 
     while (k < str3.length) {
       const n = str3[k];
 
-      // if a duplicate is found, place the state on the stack
       if (str1[i] === str2[j]) {
-        // check if this state has already been found; skip if it has
         if (!state_cache.includes(k)) {
           state_arr.push({
-            // Note: need to switch positions to test the second option
             str1: str2,
             i: j,
             str2: str1,
@@ -60,7 +63,6 @@ function checkString(str1, str2, str3) {
 
       if      (n === str1[i]) i++;
       else if (n === str2[j]) j++;
-      // a false condition found, but continue looking in different states
       else if ( state_arr.length ) break;
       else return false;
 
