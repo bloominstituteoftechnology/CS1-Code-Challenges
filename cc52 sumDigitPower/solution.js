f0be0a8674bd196f6025f4345c29b7f1717e4bbf
mixelpixel <mixelpix@mac.com>
2017-09-27 13:14:42 -0400

    cc52

--- cc52 sumDigitPower/solution.js
+++ cc52 sumDigitPower/solution.js
@@ -1,2 +1,18 @@
 //
 //
+
+function sumDigPower (a, b) {
+  const arr = [];
+  function check(n) {
+    let str = n.toString();
+    let ans = 0;
+    for (let i = 0; i < str.length; i++) {
+      ans += Math.pow(str[i], i + 1);
+    }
+    return ans === n;
+  }
+  for (let i = a; i <= b; i++) {
+    if (check(i)) arr.push(i);
+  }
+  return arr;
+}
