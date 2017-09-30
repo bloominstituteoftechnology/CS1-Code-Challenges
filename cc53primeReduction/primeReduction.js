46c4b8bad929dc87f257a0fd96508a616894a9e4
mixelpixel <mixelpix@mac.com>
2017-09-30 09:48:18 -0400

    cc53

--- cc53primeReduction/primeReduction.js
+++ cc53primeReduction/primeReduction.js
@@ -1,12 +1,9 @@
 /* c53 primeReduction https://repl.it/student/submissions/1695186
-
 Consider the prime number 23.
 If we sum the square of its digits we get: 2^2 + 3^2 = 13,
 then for 13: 1^2 + 3^2 = 10, and finally for 10: 1^2 + 0^2 = 1.
-
 Similarly, if we start with prime number 7,
 the sequence is: 7-> 49-> 97-> 130-> 10-> 1.
-
 Given a range, how many primes within that range have this property?
 */
 
