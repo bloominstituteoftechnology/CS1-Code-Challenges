56b7abb62840251ef0800fac97aecb3fde19dbfe
mixelpixel <mixelpix@mac.com>
2017-10-05 13:36:52 -0400

    cc56

--- cc56mergeSort/solution.js
+++ cc56mergeSort/solution.js
@@ -26,3 +26,38 @@ function mergeSort(arr) {
 
   return merge(mergeSort(leftArray), mergeSort(rightArray));
 }
+
+
+// Ryan's soloosh
+function mergeSort(arr) { // [3, 2],  [1, 0, -1]
+  // base case if length of arr < 2; return;
+  // break arr into halves Left & Right;
+  // Lsorted = mergeSort ()
+  if (arr.length < 2) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const leftArray = arr.slice(0, mid);
+  const rightArray = arr.slice(mid);
+
+  const merge = (leftArray, rightArray) => {
+    const mergedArray = [];
+    let leftPointer = 0;
+    let rightPointer = 0;
+
+    while (leftPointer < leftArray.length && rightPointer < rightArray.length) {
+
+     if (leftArray[leftPointer] < rightArray[rightPointer]) {
+       mergedArray.push(leftArray[leftPointer]);
+       leftPointer ++;
+       continue;
+     }
+     mergedArray.push(rightArray[rightPointer]);
+     rightPointer ++;
+    }
+
+    return mergedArray
+      .concat(leftArray.slice(leftPointer))
+      .concat(rightArray.slice(rightPointer));
+  };
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
