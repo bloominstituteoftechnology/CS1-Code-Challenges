98d3d22e576c4d1d87002fdcbf85454d208e5dce
mixelpixel <mixelpix@mac.com>
2017-10-11 11:48:36 -0400

    cc59

--- cc59nthFibonacci/solution.c
+++ cc59nthFibonacci/solution.c
@@ -1,13 +1,15 @@
 // https://repl.it/student/assignments/466973/model_solution?fromSubmissionId=1773343
-//
+// https://youtu.be/5qLwPFzqZh8
+// https://piazza.com/class/j63w1pdyopf7kj?cid=67
 
 #include <stdio.h>
 #include <assert.h>
 
 int main(int argc, char* argv[])
 {
-  int prevPrev, prev, current, n;
+  int n;
   size_t i;
+  long long prevPrev, prev, current;
 
   printf("Enter an integer: \n");
   scanf("%d", &n);
@@ -28,7 +30,7 @@ int main(int argc, char* argv[])
     prev = current;
   }
 
-  printf("Nth Fibonacci number is %d\n", current);
+  printf("Nth Fibonacci number is %lld\n", current);
 
   return 0;
 }
