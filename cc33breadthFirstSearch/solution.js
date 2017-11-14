20f6e10205bb51ac5d5439685ae8ab6dfcf94b32
mixelpixel <mixelpix@mac.com>
2017-11-14 13:02:11 -0500

    cc33

--- cc33breadthFirstSearch/solution.js
+++ cc33breadthFirstSearch/solution.js
@@ -1,7 +1,9 @@
+/* eslint no-console: 0 */
+
 const breadthFirstSearch = (tree, searchTerm) => {
   let queue = Object.values(tree);
   while (queue.length > 0) {
-    value = queue.shift();
+    let value = queue.shift();
     if (value === searchTerm) return true;
     if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
       queue = queue.concat(Object.values(value));
@@ -9,3 +11,18 @@ const breadthFirstSearch = (tree, searchTerm) => {
   }
   return false;
 };
+
+// TEST SUITE
+const tree = {
+  x: 1,
+  y: 1,
+  z: {
+    x: 1,
+    y: 1,
+    z: 1,
+  },
+  a: 2,
+};
+
+console.log(Object.values(tree));
+console.log(breadthFirstSearch(tree, 2));
