afd15f0a924967ba2f01f4b848366b5e4efaa5dd
mixelpixel <mixelpix@mac.com>
2018-02-19 21:38:10 -0500

    cc12

--- cc12operators/operators.js
+++ cc12operators/operators.js
@@ -11,7 +11,9 @@
 
 const multiply = function(a, b) {
   const arr = new Array(b).fill(a);
+  console.log(arr);
   const product = arr.reduce((a, b) => a + b, 0);
+  console.log(product);
   return product;
 };
 
@@ -36,16 +38,17 @@ const modulo = function(phi, psi) {
 
 // TEST SUITE
 // multiplication
-console.log(multiply(2, 10)); // ---> 20
-console.log(multiply(1, 1));  // ---> 1
-console.log(multiply(21, 0));
-// console.log(multiply(21, -1));
-// console.log(multiply(-21, 1));
-// console.log(multiply(-21, -3));
+console.log(multiply(2, 10));   // ---> 20
+console.log(multiply(1, 1));    // ---> 1
+console.log(multiply(21, 0));   // -->  0
+// console.log(multiply(21, -1));  // --> error
+console.log(multiply(-21, 1));  // --> -21
+// console.log(multiply(-21, -3)); // --> error
 
 // division
 console.log(divide(121, 11)); // ---> 11 remainder 0
 console.log(divide(123, 11)); // ---> 11 remainder 2
+
 // modulo
 console.log(modulo(15000, 11)); // ---> 7
-console.log(modulo(123, 11)); // ---> 2
+console.log(modulo(123, 11));   // ---> 2
